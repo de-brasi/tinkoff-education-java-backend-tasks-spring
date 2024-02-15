8092c6b8c44220f0508c9557ecfca8964f0ad8bb
de_brasi <ya.kakadu.ru@yandex.ru>
2024-02-15 17:38:01 +0300

    add context (CommandCallContext instance) as argument

--- bot/src/main/java/edu/java/bot/core/commands/TelegramBotCommandCallAction.java
+++ bot/src/main/java/edu/java/bot/core/commands/TelegramBotCommandCallAction.java
@@ -1,8 +1,9 @@
 package edu.java.bot.core.commands;
 
 import com.pengrad.telegrambot.TelegramBot;
+import edu.java.bot.entities.CommandCallContext;
 
 @FunctionalInterface
 public interface TelegramBotCommandCallAction {
-    public void call(TelegramBot targetBot);
+    public void call(TelegramBot targetBot, CommandCallContext context);
 }

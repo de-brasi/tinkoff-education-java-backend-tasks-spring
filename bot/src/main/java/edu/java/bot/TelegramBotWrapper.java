bb687c369c0599c19374d42e4480ef97006ccde8
de_brasi <ya.kakadu.ru@yandex.ru>
2024-02-15 19:06:40 +0300

    init

---
+++ bot/src/main/java/edu/java/bot/TelegramBotWrapper.java
@@ -0,0 +1,21 @@
+package edu.java.bot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
+
+public class TelegramBotWrapper extends TelegramBot {
+    public TelegramBotWrapper(String botToken) {
+        super(botToken);
+    }
+
+    public void sendPlainTextMessage(Long chatId, String message) {
+        SendMessage request = new SendMessage(chatId, message);
+        BaseResponse response = this.execute(request);
+
+        if (!response.isOk()) {
+            // todo: better logging
+            System.out.println("Failure when try to send message " + message + " to chat " + chatId);
+        }
+    }
+}

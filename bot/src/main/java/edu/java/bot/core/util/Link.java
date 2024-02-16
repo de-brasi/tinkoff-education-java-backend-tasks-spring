9d741a5b779a9782d573b8f10eadca5f2934a567
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-16 18:36:42 +0300

    better url validation

--- bot/src/main/java/edu/java/bot/core/util/Link.java
+++ bot/src/main/java/edu/java/bot/core/util/Link.java
@@ -1,10 +1,22 @@
 package edu.java.bot.core.util;
 
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
 public class Link {
     protected Link() {}
 
-    public static boolean validate(String url) {
-        // todo
-        return true;
+    public static boolean validate(String urlString) {
+        try {
+            URL url = URI.create(urlString).toURL();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            System.out.println(responseCode);
+            return !(400 <= responseCode && responseCode < 500);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }

c572aa9c552ea6ba8a1ad507be5478caef4a35a5
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-16 21:12:06 +0300

    add equals and hashCode methods

--- bot/src/main/java/edu/java/bot/entities/CommandCallContext.java
+++ bot/src/main/java/edu/java/bot/entities/CommandCallContext.java
@@ -4,10 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import java.util.List;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Getter public class CommandCallContext {
     private User user;
     private Long chatId;
     private Command command;
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CommandCallContext that = (CommandCallContext) o;
+        return Objects.equals(user, that.user) && Objects.equals(chatId, that.chatId) &&
+            Objects.equals(command, that.command);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, chatId, command);
+    }
 }

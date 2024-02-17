a3bbb167e546fa88d97656a4f673734e817668d1
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-17 21:09:33 +0300

    catch case when token in env not found

--- bot/src/main/java/edu/java/bot/services/TelegramBotWrapper.java
+++ bot/src/main/java/edu/java/bot/services/TelegramBotWrapper.java
@@ -9,6 +9,20 @@ public class TelegramBotWrapper extends TelegramBot {
         super(botToken);
     }
 
+    public static TelegramBotWrapper createBotWithTokenFromEnv(String variableName) {
+        String token = System.getenv(variableName);
+
+        if (token == null) {
+            throw new RuntimeException(String.format(
+                "Environment variable with name %s not found! " +
+                    "Make sure you set the Telegram token value in the environment variables by this name",
+                variableName
+            ));
+        }
+
+        return new TelegramBotWrapper(token);
+    }
+
     public void sendPlainTextMessage(Long chatId, String message) {
         SendMessage request = new SendMessage(chatId, message);
         BaseResponse response = this.execute(request);

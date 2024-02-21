cfbb72aee4c45fd5ed00064b05bdbcd0081b0c2f
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-21 19:17:54 +0300

    delete debug methods; add realization to "fetchUpdate" method

--- scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
@@ -10,58 +10,42 @@ import java.time.ZoneOffset;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class StackOverflowClient  {
+public class StackOverflowClient {
     private final RestClient restClient;
 
     private final static String defaultBaseUrl =
         "https://api.stackexchange.com/2.3/questions/";
 
     public StackOverflowClient(RestClient.Builder restClientBuilder) {
-        this.restClient = restClientBuilder.requestFactory(new ReactorNettyClientRequestFactory()).baseUrl(StackOverflowClient.defaultBaseUrl).build();
+        this.restClient = restClientBuilder.requestFactory(new ReactorNettyClientRequestFactory())
+            .baseUrl(StackOverflowClient.defaultBaseUrl).build();
     }
 
     public StackOverflowClient(RestClient.Builder restClientBuilder, String baseUrl) {
-        this.restClient = restClientBuilder.requestFactory(new ReactorNettyClientRequestFactory()).baseUrl(baseUrl).build();
+        this.restClient =
+            restClientBuilder.requestFactory(new ReactorNettyClientRequestFactory()).baseUrl(baseUrl).build();
     }
 
-    public String getAllInfo(String questionId) {
-        return this.restClient
+    public UpdateResponse fetchUpdate(Integer questionId) {
+        String responseBody = this.restClient
             .get()
             .uri("/%s?site=stackoverflow&filter=withbody".formatted(questionId))
             .header(HttpHeaders.ACCEPT_ENCODING, "gzip")
             .retrieve()
             .body(String.class);
-    }
-
-    public String getAllInfo(Integer questionId) {
-        return this.getAllInfo(Integer.toString(questionId));
-    }
-
-    public String debugGetLastActivityTimestampAsString(Integer questionId) {
-        String responseBody = this.getAllInfo(Integer.toString(questionId));
-        System.out.println(responseBody);
 
         Pattern dateSearchPattern = Pattern.compile("\"last_activity_date\":\\s*([0-9]+)");
+        assert responseBody != null;
         Matcher matcher = dateSearchPattern.matcher(responseBody);
 
-        if (matcher.find()) {
-            return matcher.group(1);
-        } else {
+        if (!matcher.find()) {
             throw new IllegalStateException("No match found for last activity date");
         }
-    }
 
-    public OffsetDateTime debugGetLastActivityTimestampAsDateTime(Integer questionId) {
-        String timestampString = debugGetLastActivityTimestampAsString(questionId);
-        long epoch = Long.parseLong(timestampString);
+        String updateTimeString = matcher.group(1);
+        long updateTimeUnixEpoch = Long.parseLong(updateTimeString);
+        OffsetDateTime updDate = Instant.ofEpochSecond(updateTimeUnixEpoch).atOffset(ZoneOffset.UTC);
 
-        Instant instant = Instant.ofEpochSecond(epoch);
-
-        return instant.atOffset(ZoneOffset.UTC);
-    }
-
-    // TODO
-    public UpdateResponse fetchUpdate() {
-        return null;
+        return new UpdateResponse(updDate);
     }
 }

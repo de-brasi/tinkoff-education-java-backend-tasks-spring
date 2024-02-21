4934ec458a8d9a6b4c1a16d111c4b91caf085a9e
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-21 18:54:32 +0300

    add functions to getting "updated_at" value as OffsetDateTime

--- scrapper/src/main/java/edu/java/clients/GitHubClient.java
+++ scrapper/src/main/java/edu/java/clients/GitHubClient.java
@@ -2,6 +2,9 @@ package edu.java.clients;
 
 import edu.java.entities.UpdateResponse;
 import org.springframework.web.client.RestClient;
+import java.time.OffsetDateTime;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GitHubClient {
 
@@ -20,6 +23,24 @@ public class GitHubClient {
 
     public String getAllInfo(String owner, String repo) {
         return this.restClient.get().uri("/{owner}/{repo}", owner, repo).retrieve().body(String.class);
+    }
+
+    public String debugGetLastActivityTimestampAsString(String owner, String repo) {
+        String responseBody = this.getAllInfo(owner, repo);
+
+        Pattern dateSearchPattern = Pattern.compile("\"updated_at\":\s*\"([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z)\"");
+        Matcher matcher = dateSearchPattern.matcher(responseBody);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            throw new IllegalStateException("No match found for last activity date");
+        }
+    }
+
+    public OffsetDateTime debugGetLastActivityTimestampAsDateTime(String owner, String repo) {
+        String src = this.debugGetLastActivityTimestampAsString(owner, repo);
+        return OffsetDateTime.parse(src);
     }
 
     // TODO

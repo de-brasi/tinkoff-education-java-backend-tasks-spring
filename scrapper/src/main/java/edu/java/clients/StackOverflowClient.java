a63e705f601415d11635c86c299d3366e6457b96
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-21 17:53:55 +0300

    add functions to getting "last_activity_date" value as timestamp (string with number), as OffsetDateTime

--- scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
@@ -4,6 +4,11 @@ import edu.java.entities.UpdateResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.client.ReactorNettyClientRequestFactory;
 import org.springframework.web.client.RestClient;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StackOverflowClient  {
     private final RestClient restClient;
@@ -30,6 +35,29 @@ public class StackOverflowClient  {
 
     public String getAllInfo(Integer questionId) {
         return this.getAllInfo(Integer.toString(questionId));
+    }
+
+    public String debugGetLastActivityTimestampAsString(Integer questionId) {
+        String responseBody = this.getAllInfo(Integer.toString(questionId));
+        System.out.println(responseBody);
+
+        Pattern dateSearchPattern = Pattern.compile("\"last_activity_date\":\\s*([0-9]+)");
+        Matcher matcher = dateSearchPattern.matcher(responseBody);
+
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            throw new IllegalStateException("No match found for last activity date");
+        }
+    }
+
+    public OffsetDateTime debugGetLastActivityTimestampAsDateTime(Integer questionId) {
+        String timestampString = debugGetLastActivityTimestampAsString(questionId);
+        long epoch = Long.parseLong(timestampString);
+
+        Instant instant = Instant.ofEpochSecond(epoch);
+
+        return instant.atOffset(ZoneOffset.UTC);
     }
 
     // TODO

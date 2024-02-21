c597d2d286df892d2b0a13ef24de6ec6e7663323
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-21 16:51:00 +0300

    solve problem with gzip request's body compression

--- scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/clients/StackOverflowClient.java
@@ -1,27 +1,29 @@
 package edu.java.clients;
 
 import edu.java.entities.UpdateResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ReactorNettyClientRequestFactory;
 import org.springframework.web.client.RestClient;
 
 public class StackOverflowClient  {
     private final RestClient restClient;
 
-    // TODO: корректный базовый URL
     private final static String defaultBaseUrl =
         "https://api.stackexchange.com/2.3/questions/";
 
     public StackOverflowClient(RestClient.Builder restClientBuilder) {
-        this.restClient = restClientBuilder.baseUrl(StackOverflowClient.defaultBaseUrl).build();
+        this.restClient = restClientBuilder.requestFactory(new ReactorNettyClientRequestFactory()).baseUrl(StackOverflowClient.defaultBaseUrl).build();
     }
 
     public StackOverflowClient(RestClient.Builder restClientBuilder, String baseUrl) {
-        this.restClient = restClientBuilder.baseUrl(baseUrl).build();
+        this.restClient = restClientBuilder.requestFactory(new ReactorNettyClientRequestFactory()).baseUrl(baseUrl).build();
     }
 
     public String getAllInfo(String questionId) {
         return this.restClient
             .get()
-            .uri("/%s?site=stackoverflow".formatted(questionId))
+            .uri("/%s?site=stackoverflow&filter=withbody".formatted(questionId))
+            .header(HttpHeaders.ACCEPT_ENCODING, "gzip")
             .retrieve()
             .body(String.class);
     }

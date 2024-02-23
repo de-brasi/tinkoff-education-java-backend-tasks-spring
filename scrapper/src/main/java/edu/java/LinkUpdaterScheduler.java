92180ca39b489db9e85eda23f44112209f328078
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-23 20:56:52 +0300

    mark LinkUpdaterScheduler as a Service; mark update as @Scheduled, fixedDelayString from bean "scheduler"

--- scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -2,10 +2,12 @@ package edu.java;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+@Service
 @EnableScheduling
 public class LinkUpdaterScheduler {
-    @Scheduled
+    @Scheduled(fixedDelayString = "#{@scheduler.interval()}")
     public void update() {
         System.out.println("Update");
     }

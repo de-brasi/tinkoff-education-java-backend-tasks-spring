1c6ca339a4c6bdd8fb65d47349209d7fdab2d6ef
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-23 21:27:38 +0300

    test case with missing field; test case with empty body;

--- scrapper/src/test/java/edu/java/scrapper/GitHubClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/GitHubClientTest.java
@@ -2,6 +2,8 @@ package edu.java.scrapper;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import edu.java.clients.GitHubClient;
+import edu.java.exceptions.EmptyResponseBodyException;
+import edu.java.exceptions.FieldNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,11 +13,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.time.OffsetDateTime;
+import java.util.concurrent.atomic.AtomicReference;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -27,9 +31,8 @@ public class GitHubClientTest {
     GitHubClient gitHubClient;
 
     @Test
-    @DisplayName("Get update time with GitHub client")
+    @DisplayName("Get update time with GitHub client; Complete response body")
     public void test1() {
-        final String testURL = "localhost";
         final String testURI = "repos";
         final String testOwner = "owner";
         final String testRepoName = "repo";
@@ -42,10 +45,57 @@ public class GitHubClientTest {
             )
         );
 
+        AtomicReference<OffsetDateTime> fetchingResult = new AtomicReference<>();
+
+        assertThatCode(
+            () -> fetchingResult.set(gitHubClient.fetchUpdate(testOwner, testRepoName).updateTime())
+        ).doesNotThrowAnyException();
+
+
         assertThat(
-            gitHubClient.fetchUpdate(testOwner, testRepoName).updateTime()
+            fetchingResult.get()
         ).isEqualTo(
             OffsetDateTime.parse("2024-02-12T12:45:18Z")
         );
+    }
+
+    @Test
+    @DisplayName("Get update time with GitHub client; Empty response body")
+    public void test2() {
+        final String testURI = "repos";
+        final String testOwner = "owner";
+        final String testRepoName = "repo";
+
+        stubFor(get("/" + testURI + "/" + testOwner + "/" + testRepoName)
+            .willReturn(
+                aResponse()
+                    .withBody("")
+                    .withStatus(200)
+            )
+        );
+
+        assertThatThrownBy(
+            () -> gitHubClient.fetchUpdate(testOwner, testRepoName).updateTime()
+        ).isInstanceOf(EmptyResponseBodyException.class);
+    }
+
+    @Test
+    @DisplayName("Get update time with GitHub client; Incomplete response body")
+    public void test3() {
+        final String testURI = "repos";
+        final String testOwner = "owner";
+        final String testRepoName = "repo";
+
+        stubFor(get("/" + testURI + "/" + testOwner + "/" + testRepoName)
+            .willReturn(
+                aResponse()
+                    .withBody("\"some_field\": \"2024-02-12T12:45:18Z\"")
+                    .withStatus(200)
+            )
+        );
+
+        assertThatThrownBy(
+            () -> gitHubClient.fetchUpdate(testOwner, testRepoName).updateTime()
+        ).isInstanceOf(FieldNotFoundException.class);
     }
 }

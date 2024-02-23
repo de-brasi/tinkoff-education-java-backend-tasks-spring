cfdcc05301a4f158495ac9cbd560695b606c5ac7
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-23 21:56:32 +0300

    test case with missing field; test case with empty body;

--- scrapper/src/test/java/edu/java/scrapper/StackoverflowClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/StackoverflowClientTest.java
@@ -2,6 +2,8 @@ package edu.java.scrapper;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import edu.java.clients.StackOverflowClient;
+import edu.java.exceptions.EmptyResponseBodyException;
+import edu.java.exceptions.FieldNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,12 +13,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.time.OffsetDateTime;
+import java.util.concurrent.atomic.AtomicReference;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.shouldHaveThrown;
 
 @ExtendWith(SpringExtension.class)
@@ -29,7 +34,7 @@ public class StackoverflowClientTest {
     StackOverflowClient stackOverflowClient;
 
     @Test
-    @DisplayName("Get update time with StackOverflow client")
+    @DisplayName("Get update time with StackOverflow client; Complete response body")
     public void test1() {
         final String testURI = "questions";
         final int testQuestionId = 123456;
@@ -44,10 +49,54 @@ public class StackoverflowClientTest {
             )
         );
 
+        AtomicReference<OffsetDateTime> fetchingResult = new AtomicReference<>();
+
+        assertThatCode(
+            () -> fetchingResult.set(stackOverflowClient.fetchUpdate(testQuestionId).updateTime())
+        ).doesNotThrowAnyException();
+
         assertThat(
-            stackOverflowClient.fetchUpdate(testQuestionId).updateTime().toEpochSecond()
+            fetchingResult.get().toEpochSecond()
         ).isEqualTo(
             testDate
         );
+    }
+
+    @Test
+    @DisplayName("Get update time with GitHub client; Empty response body")
+    public void test2() {
+        final String testURI = "questions";
+        final int testQuestionId = 123456;
+
+        stubFor(get(urlMatching("/" + testURI + "/" + testQuestionId + "\\??(.*)"))
+            .willReturn(
+                aResponse()
+                    .withBody("")
+                    .withStatus(200)
+            )
+        );
+
+        assertThatThrownBy(
+            () -> stackOverflowClient.fetchUpdate(testQuestionId).updateTime()
+        ).isInstanceOf(Exception.class);
+    }
+
+    @Test
+    @DisplayName("Get update time with GitHub client; Incomplete response body")
+    public void test3() {
+        final String testURI = "questions";
+        final int testQuestionId = 123456;
+
+        stubFor(get(urlMatching("/" + testURI + "/" + testQuestionId + "\\??(.*)"))
+            .willReturn(
+                aResponse()
+                    .withBody("\"some_field\": \"2024-02-12T12:45:18Z\"")
+                    .withStatus(200)
+            )
+        );
+
+        assertThatThrownBy(
+            () -> stackOverflowClient.fetchUpdate(testQuestionId).updateTime()
+        ).isInstanceOf(FieldNotFoundException.class);
     }
 }

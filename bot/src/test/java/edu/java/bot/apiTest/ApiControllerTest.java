31f63570075bf0da6af27cb71cab966eda23982e
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-26 21:06:26 +0300

    init tests: 1) correct request; 2) request with empty body; 3) simple test when ChatIdNotExistsException occurs; 4) simple test when ReAddingLinkException occurs; 5) simple test when ReRegistrationException occurs;

---
+++ bot/src/test/java/edu/java/bot/apiTest/ApiControllerTest.java
@@ -0,0 +1,120 @@
+package edu.java.bot.apiTest;
+
+import edu.java.bot.api.Controllers;
+import edu.java.bot.api.dtos.LinkUpdateRequest;
+import edu.java.bot.api.exceptions.ChatIdNotExistsException;
+import edu.java.bot.api.exceptions.ReAddingLinkException;
+import edu.java.bot.api.exceptions.ReRegistrationException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Controllers controllers;
+
+    @Test
+    @DisplayName("Test correct (stub) request")
+    public void test1() throws Exception {
+        final String jsonRequest = "{\"id\": 0,\"url\": \"test\",\"description\": \"test\",\"tgChatIds\": null}";
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                    .post("/bot/api/updates")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest)
+            )
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Test empty body request")
+    public void test2() throws Exception {
+        final String jsonRequest = "";
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                    .post("/bot/api/updates")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest)
+            )
+            .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @DisplayName("Test response when ChatIdNotExistsException occurs")
+    public void test3() throws Exception {
+        final String jsonRequest = "{\"id\": 0,\"url\": \"test\",\"description\": \"test\",\"tgChatIds\": null}";
+
+        final String exceptionMessage = "test message";
+
+        when(controllers.handleUpdateRequest(any(LinkUpdateRequest.class)))
+            .thenAnswer(invocation -> {
+                throw new ChatIdNotExistsException(exceptionMessage);
+            });
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                    .post("/bot/api/updates")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest)
+            )
+            .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @DisplayName("Test response when ReAddingLinkException occurs")
+    public void test4() throws Exception {
+        final String jsonRequest = "{\"id\": 0,\"url\": \"test\",\"description\": \"test\",\"tgChatIds\": null}";
+
+        final String exceptionMessage = "test message";
+
+        when(controllers.handleUpdateRequest(any(LinkUpdateRequest.class)))
+            .thenAnswer(invocation -> {
+                throw new ReAddingLinkException(exceptionMessage);
+            });
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                    .post("/bot/api/updates")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest)
+            )
+            .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @DisplayName("Test response when ReRegistrationException occurs")
+    public void test5() throws Exception {
+        final String jsonRequest = "{\"id\": 0,\"url\": \"test\",\"description\": \"test\",\"tgChatIds\": null}";
+
+        final String exceptionMessage = "test message";
+
+        when(controllers.handleUpdateRequest(any(LinkUpdateRequest.class)))
+            .thenAnswer(invocation -> {
+                throw new ReRegistrationException(exceptionMessage);
+            });
+
+        mockMvc.perform(
+                MockMvcRequestBuilders
+                    .post("/bot/api/updates")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(jsonRequest)
+            )
+            .andExpect(status().is4xxClientError());
+    }
+}

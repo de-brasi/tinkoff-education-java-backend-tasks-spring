bab6640898ba05b6bc96545ede988d96cd820f4e
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-27 13:00:31 +0300

    delete extra error handlers

--- bot/src/main/java/edu/java/bot/api/ControllersExceptionHandler.java
+++ bot/src/main/java/edu/java/bot/api/ControllersExceptionHandler.java
@@ -2,8 +2,6 @@ package edu.java.bot.api;
 
 import edu.java.bot.api.dtos.ApiErrorResponse;
 import edu.java.bot.api.exceptions.ChatIdNotExistsException;
-import edu.java.bot.api.exceptions.ReAddingLinkException;
-import edu.java.bot.api.exceptions.ReRegistrationException;
 import java.util.Arrays;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -22,38 +20,6 @@ public class ControllersExceptionHandler {
                 "chat id not exists stub error",
                 "000",
                 "ChatIdNotExistsException",
-                e.getMessage(),
-                Arrays.stream(e.getStackTrace())
-                    .map(StackTraceElement::toString)
-                    .toList()
-            ),
-            HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value())
-        );
-    }
-
-    @ExceptionHandler(ReAddingLinkException.class)
-    public ResponseEntity<ApiErrorResponse> doubleAddedLinkExceptionHandler(Exception e) {
-        return new ResponseEntity<>(
-            new ApiErrorResponse(
-                "double adding link stub error",
-                "000",
-                "ReAddingLinkException",
-                e.getMessage(),
-                Arrays.stream(e.getStackTrace())
-                    .map(StackTraceElement::toString)
-                    .toList()
-            ),
-            HttpStatusCode.valueOf(HttpStatus.BAD_REQUEST.value())
-        );
-    }
-
-    @ExceptionHandler(ReRegistrationException.class)
-    public ResponseEntity<ApiErrorResponse> doubleRegistrationExceptionHandler(Exception e) {
-        return new ResponseEntity<>(
-            new ApiErrorResponse(
-                "double registration stub error",
-                "000",
-                "ReRegistrationException",
                 e.getMessage(),
                 Arrays.stream(e.getStackTrace())
                     .map(StackTraceElement::toString)

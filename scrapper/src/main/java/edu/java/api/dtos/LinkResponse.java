71145c7f7c2c0a14351e03bd22f25ad86f5511b8
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-27 12:46:49 +0300

    add lombok annotations

--- scrapper/src/main/java/edu/java/api/dtos/LinkResponse.java
+++ scrapper/src/main/java/edu/java/api/dtos/LinkResponse.java
@@ -1,5 +1,10 @@
 package edu.java.api.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class LinkResponse {
     long id;
     String url;
--- scrapper/src/main/java/edu/java/api/dtos/ListLinksResponse.java
+++ scrapper/src/main/java/edu/java/api/dtos/ListLinksResponse.java
@@ -1,7 +1,11 @@
 package edu.java.api.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 public class ListLinksResponse {
     List<LinkResponse> links;
     int size;

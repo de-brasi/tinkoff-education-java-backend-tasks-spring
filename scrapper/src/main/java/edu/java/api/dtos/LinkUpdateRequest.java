04a49f1bb9e6f4a5e9dff44152f7161766718b54
de-brasi <leskin.ilia.public@yandex.ru>
2024-02-29 22:15:24 +0300

    init; dto for bot requesting

---
+++ scrapper/src/main/java/edu/java/api/dtos/LinkUpdateRequest.java
@@ -0,0 +1,13 @@
+package edu.java.api.dtos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class LinkUpdateRequest {
+    int id;
+    String url;
+    String description;
+    List<Integer> tgChatIds;
+}

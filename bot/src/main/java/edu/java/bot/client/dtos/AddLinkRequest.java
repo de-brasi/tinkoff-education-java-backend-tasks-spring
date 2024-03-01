f0c91bd389ccd44b3833ca2feca9c1f3a1860f6e
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-01 21:10:47 +0300

    add lombok @NoArgsConstructor

--- bot/src/main/java/edu/java/bot/client/dtos/AddLinkRequest.java
+++ bot/src/main/java/edu/java/bot/client/dtos/AddLinkRequest.java
@@ -2,9 +2,11 @@ package edu.java.bot.client.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AddLinkRequest {
     String link;
 }
--- bot/src/main/java/edu/java/bot/client/dtos/ListLinksResponse.java
+++ bot/src/main/java/edu/java/bot/client/dtos/ListLinksResponse.java
@@ -2,11 +2,13 @@ package edu.java.bot.client.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ListLinksResponse {
     List<LinkResponse> links;
     int size;

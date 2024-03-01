a8009a05658535a77d88ed164f09f8a5344e3668
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-01 21:56:10 +0300

    init

---
+++ bot/src/main/java/edu/java/bot/client/dtos/RemoveLinkRequest.java
@@ -0,0 +1,12 @@
+package edu.java.bot.client.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RemoveLinkRequest {
+    String link;
+}

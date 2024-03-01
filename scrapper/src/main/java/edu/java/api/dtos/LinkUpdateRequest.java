10663844c69438e44b1f615e99b2a6f444e0489d
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-01 12:47:48 +0300

    add all args ctor with lombok

--- scrapper/src/main/java/edu/java/api/dtos/LinkUpdateRequest.java
+++ scrapper/src/main/java/edu/java/api/dtos/LinkUpdateRequest.java
@@ -1,10 +1,12 @@
 package edu.java.api.dtos;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class LinkUpdateRequest {
     int id;
     String url;

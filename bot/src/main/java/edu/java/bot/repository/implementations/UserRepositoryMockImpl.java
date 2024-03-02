1bba5fee360321f6d6c5925f21a1af9cfd4b7b79
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-02 21:20:34 +0300

    mark as a component

--- bot/src/main/java/edu/java/bot/repository/implementations/UserRepositoryMockImpl.java
+++ bot/src/main/java/edu/java/bot/repository/implementations/UserRepositoryMockImpl.java
@@ -4,7 +4,9 @@ import edu.java.bot.repository.interfaces.UsersRepository;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserRepositoryMockImpl implements UsersRepository {
     @Override
     public void storeLinksForUser(Long userTelegramId, List<String> links) {

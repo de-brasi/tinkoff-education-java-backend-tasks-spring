1d875d8399ab24a47749384a6dbc4a3957d29a18
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-02 12:14:12 +0300

    in "sendUpdates" method add status handlers for 1xx, 3xx, 4xx, 5xx

--- scrapper/src/main/java/edu/java/client/BotClient.java
+++ scrapper/src/main/java/edu/java/client/BotClient.java
@@ -1,12 +1,13 @@
 package edu.java.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.common.dtos.ApiErrorResponse;
 import edu.common.dtos.LinkUpdateRequest;
 import edu.common.exceptions.IncorrectRequestException;
-import org.springframework.http.ResponseEntity;
+import edu.common.exceptions.UnexpectedResponse;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
 import java.util.List;
-import java.util.Objects;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 public class BotClient {
@@ -18,6 +19,49 @@ public class BotClient {
 
     private final static String ENDPOINT_UPDATES = "/updates";
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final RestClient.ResponseSpec.ErrorHandler ENDPOINT_UPDATES_STATUS_1xx_HANDLER = (req, resp) -> {
+        ApiErrorResponse errorResponse = objectMapper.readValue(
+            new String(resp.getBody().readAllBytes()), ApiErrorResponse.class
+        );
+        throw new UnexpectedResponse(
+            resp.getStatusCode().value(),
+            errorResponse.getExceptionMessage()
+        );
+    };
+
+    private final RestClient.ResponseSpec.ErrorHandler ENDPOINT_UPDATES_STATUS_3xx_HANDLER = (req, resp) -> {
+        ApiErrorResponse errorResponse = objectMapper.readValue(
+            new String(resp.getBody().readAllBytes()), ApiErrorResponse.class
+        );
+        throw new UnexpectedResponse(
+            resp.getStatusCode().value(),
+            errorResponse.getExceptionMessage()
+        );
+    };
+
+    private final RestClient.ResponseSpec.ErrorHandler ENDPOINT_UPDATES_STATUS_4xx_HANDLER = (req, resp) -> {
+        ApiErrorResponse errorResponse = objectMapper.readValue(
+            new String(resp.getBody().readAllBytes()), ApiErrorResponse.class
+        );
+        if (resp.getStatusCode().value() == 400) {
+            throw new IncorrectRequestException(errorResponse.getExceptionMessage());
+        } else {
+            throw new UnexpectedResponse(resp.getStatusCode().value(), errorResponse.getExceptionMessage());
+        }
+    };
+
+    private final RestClient.ResponseSpec.ErrorHandler ENDPOINT_UPDATES_STATUS_5xx_HANDLER = (req, resp) -> {
+        ApiErrorResponse errorResponse = objectMapper.readValue(
+            new String(resp.getBody().readAllBytes()), ApiErrorResponse.class
+        );
+        throw new UnexpectedResponse(
+            resp.getStatusCode().value(),
+            errorResponse.getExceptionMessage()
+        );
+    };
+
     public BotClient(RestClient.Builder restClientBuilder) {
         this.restClient = restClientBuilder.baseUrl(BotClient.DEFAULT_BASE_URL).build();
     }
@@ -26,28 +70,20 @@ public class BotClient {
         this.restClient = restClientBuilder.baseUrl(baseUrl).build();
     }
 
-    // TODO: исправить обработку ошибок
     public void sendUpdates(
         int id, String updatedUrl, String updateDescription, List<Integer> subscribers
     ) {
         LinkUpdateRequest updates = new LinkUpdateRequest(id, updatedUrl, updateDescription, subscribers);
-        ResponseEntity<ApiErrorResponse> response = this.restClient
+        this.restClient
             .post()
             .uri(BotClient.ENDPOINT_UPDATES)
             .contentType(APPLICATION_JSON)
             .body(updates)
             .retrieve()
-            .toEntity(ApiErrorResponse.class);
-
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            // todo: better logging
-            System.out.println(response.getBody());
-
-            // todo отличать ошибку 400 от 404 и тд, то есть сделать как в клиенте bot
-            throw new IncorrectRequestException(
-                Objects.requireNonNull(response.getBody()).getExceptionMessage()
-            );
-        }
+            .onStatus(HttpStatusCode::is1xxInformational, ENDPOINT_UPDATES_STATUS_1xx_HANDLER)
+            .onStatus(HttpStatusCode::is3xxRedirection, ENDPOINT_UPDATES_STATUS_3xx_HANDLER)
+            .onStatus(HttpStatusCode::is4xxClientError, ENDPOINT_UPDATES_STATUS_4xx_HANDLER)
+            .onStatus(HttpStatusCode::is5xxServerError, ENDPOINT_UPDATES_STATUS_5xx_HANDLER);
     }
 
 }

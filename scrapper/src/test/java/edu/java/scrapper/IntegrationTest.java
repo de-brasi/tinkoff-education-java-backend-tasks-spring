52b8b4e22dd80f37c4f08d3f906470b4ede6d9fa
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-09 20:14:55 +0300

    runMigrations realization

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,10 +1,23 @@
 package edu.java.scrapper;
 
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import liquibase.resource.DirectoryResourceAccessor;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Testcontainers
 public abstract class IntegrationTest {
@@ -21,7 +34,37 @@ public abstract class IntegrationTest {
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        final String jdbcUrl = c.getJdbcUrl();
+        final String username = c.getUsername();
+        final String password = c.getPassword();
+
+        final String changelogFilePath = new File(".")
+            .toPath().toAbsolutePath()
+            .getParent().getParent()
+            .resolve("migrations").resolve("master.xml")
+            .toString();
+
+        try (
+            java.sql.Connection connection = DriverManager.getConnection(jdbcUrl, username, password)
+        ) {
+            Database database =
+                DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Liquibase liquibase = new liquibase.Liquibase(
+                "master.xml",
+                new DirectoryResourceAccessor(
+                    new File(".")
+                        .toPath().toAbsolutePath()
+                        .getParent().getParent()
+                        .resolve("migrations")
+                ),
+                database
+            );
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (SQLException | LiquibaseException | FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @DynamicPropertySource

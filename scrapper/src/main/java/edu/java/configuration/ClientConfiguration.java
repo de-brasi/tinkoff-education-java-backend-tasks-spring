d199c2bc7f3ec0ae79801b540381b71afc3967d7
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-11 21:51:56 +0300

    linter fix

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SuppressWarnings("MagicNumber")
 public class ClientConfiguration {
     @Bean("gitHubClient")
     public GitHubClient gitHubClient() {

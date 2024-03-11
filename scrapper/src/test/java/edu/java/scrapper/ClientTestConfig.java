f7d8fe4636ace25ebb51962f8eab01fc78711071
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-11 19:39:50 +0300

    manually creating RestClient builder instead of autowiring

--- scrapper/src/test/java/edu/java/scrapper/ClientTestConfig.java
+++ scrapper/src/test/java/edu/java/scrapper/ClientTestConfig.java
@@ -2,18 +2,19 @@ package edu.java.scrapper;
 
 import edu.java.clients.GitHubClient;
 import edu.java.clients.StackOverflowClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
 public class ClientTestConfig {
     @Bean("testGitHubClient")
-    public GitHubClient gitHubClient(@Autowired RestClient.Builder builder) {
+    public GitHubClient gitHubClient() {
+        RestClient.Builder builder = RestClient.builder();
         return new GitHubClient(builder, "http://localhost:8080/repos/");
     }
 
     @Bean("testStackOverflowClient")
-    public StackOverflowClient stackoverflowClient(@Autowired RestClient.Builder builder) {
+    public StackOverflowClient stackoverflowClient() {
+        RestClient.Builder builder = RestClient.builder();
         return new StackOverflowClient(builder, "http://localhost:8080/questions/");
     }
 }

9a44906207bf61340d92892b982a14a1a498fc03
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-11 17:24:00 +0300

    delete

--- bot/src/main/java/edu/java/bot/customexceptions/NullTelegramTokenException.java
+++
@@ -1,7 +0,0 @@
-package edu.java.bot.customexceptions;
-
-public class NullTelegramTokenException extends Exception {
-    public NullTelegramTokenException(String errorMessage) {
-        super(errorMessage);
-    }
-}

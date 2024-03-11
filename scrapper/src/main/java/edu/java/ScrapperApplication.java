30c92dcb3fd07de92fd2a070424cb2800e960e8b
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-11 17:40:23 +0300

    discard test

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -1,10 +1,6 @@
 package edu.java;
 
-import edu.java.clients.GitHubClient;
-import edu.java.clients.StackOverflowClient;
 import edu.java.configuration.ApplicationConfig;
-import edu.java.exceptions.EmptyResponseBodyException;
-import edu.java.exceptions.FieldNotFoundException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -12,23 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class ScrapperApplication {
-    @SuppressWarnings("RegexpSinglelineJava")
-    public static void main(String[] args) throws EmptyResponseBodyException, FieldNotFoundException {
-        var context = SpringApplication.run(ScrapperApplication.class, args);
-
-        // бин gitHubClient из контекста
-        GitHubClient githubClient =
-            context.getBean("gitHubClient", GitHubClient.class);
-
-        // результат
-        System.out.println(
-            "GitHub client: "
-                + githubClient.fetchUpdate("de-brasi", "tinkoff-education-java-backend-tasks-spring")
-        );
-
-        StackOverflowClient stackoverflowClient =
-            context.getBean("stackOverflowClient", StackOverflowClient.class);
-        final int questionId = 41808152;
-        System.out.println("StackoverflowClient: " + stackoverflowClient.fetchUpdate(questionId));
+    public static void main(String[] args) {
+        SpringApplication.run(ScrapperApplication.class, args);
     }
 }

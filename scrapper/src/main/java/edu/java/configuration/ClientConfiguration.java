66795d738f8e7ee428779c36dad95494faf3885d
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-12 11:56:15 +0300

    using timeout values from config beans

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -2,6 +2,7 @@ package edu.java.configuration;
 
 import edu.java.clients.GitHubClient;
 import edu.java.clients.StackOverflowClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,14 +10,12 @@ import org.springframework.context.annotation.Configuration;
 @SuppressWarnings("MagicNumber")
 public class ClientConfiguration {
     @Bean("gitHubClient")
-    public GitHubClient gitHubClient() {
-        // todo: timeout value from config
-        return new GitHubClient("https://api.github.com/repos/", 1000);
+    public GitHubClient gitHubClient(@Value("#{@githubClientSettings.timeInMilliseconds()}") int timeoutInMilliseconds) {
+        return new GitHubClient("https://api.github.com/repos/", timeoutInMilliseconds);
     }
 
     @Bean("stackOverflowClient")
-    public StackOverflowClient stackoverflowClient() {
-        // todo: timeout value from config
-        return new StackOverflowClient(1000);
+    public StackOverflowClient stackoverflowClient(@Value("#{@stackoverflowClientSettings.timeInMilliseconds()}") int timeoutInMilliseconds) {
+        return new StackOverflowClient(timeoutInMilliseconds);
     }
 }

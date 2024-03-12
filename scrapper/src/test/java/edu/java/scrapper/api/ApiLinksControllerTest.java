85926c121e6c5ee2987238ca8d3cc90506e13011
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-12 17:16:54 +0300

    rename; discard tests for chat endpoint

--- scrapper/src/test/java/edu/java/scrapper/api/ApiLinksControllerTest.java
+++ scrapper/src/test/java/edu/java/scrapper/api/ApiLinksControllerTest.java
@@ -1,9 +1,8 @@
 package edu.java.scrapper.api;
 
-import edu.common.exceptions.ChatIdNotExistsException;
 import edu.common.exceptions.LinkNotExistsException;
-import edu.common.exceptions.ReRegistrationException;
-import edu.java.api.Controllers;
+import edu.java.api.ChatController;
+import edu.java.api.LinksController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,131 +19,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ApiControllerTest {
+public class ApiLinksControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @MockBean
-    private Controllers controllers;
+    private ChatController chatController;
 
-    // POST: /scrapper/api/tg-chat/{id}
-    @Test
-    @DisplayName("Test correct chat registry in /tg-chat")
-    public void test1() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .post("/scrapper/api/tg-chat/1")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("Test correct chat registry to /tg-chat with manually occurred Exception")
-    public void test2() throws Exception {
-        when(controllers.handleRegistryChat(any(Long.class)))
-            .thenAnswer(invocation -> {
-                throw new Exception();
-            });
-
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .post("/scrapper/api/tg-chat/1")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().is5xxServerError());
-    }
-
-    @Test
-    @DisplayName("Test correct chat registry to /tg-chat with manually occurred ReRegistrationException")
-    public void test3() throws Exception {
-        when(controllers.handleRegistryChat(any(Long.class)))
-            .thenAnswer(invocation -> {
-                throw new ReRegistrationException();
-            });
-
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .post("/scrapper/api/tg-chat/1")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("Test incorrect chat registry to /tg-chat with unexpected argument datatype")
-    public void test4() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .post("/scrapper/api/tg-chat/one")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().isBadRequest());
-    }
-
-    // DELETE: /scrapper/api/tg-chat/{id}
-    @Test
-    @DisplayName("Test correct chat delete in /tg-chat")
-    public void test5() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .delete("/scrapper/api/tg-chat/1")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().isOk());
-    }
-
-    @Test
-    @DisplayName("Test correct chat delete to /tg-chat with manually occurred Exception")
-    public void test6() throws Exception {
-        when(controllers.handleDeleteChat(any(Long.class)))
-            .thenAnswer(invocation -> {
-                throw new Exception();
-            });
-
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .delete("/scrapper/api/tg-chat/1")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().isInternalServerError());
-    }
-
-    @Test
-    @DisplayName("Test correct chat delete to /tg-chat with manually occurred ChatIdNotExistsException")
-    public void test7() throws Exception {
-        when(controllers.handleDeleteChat(any(Long.class)))
-            .thenAnswer(invocation -> {
-                throw new ChatIdNotExistsException();
-            });
-
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .delete("/scrapper/api/tg-chat/1")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("Test incorrect chat delete to /tg-chat with unexpected argument datatype")
-    public void test8() throws Exception {
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                    .delete("/scrapper/api/tg-chat/one")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content("")
-            )
-            .andExpect(status().isBadRequest());
-    }
+    @MockBean
+    private LinksController linksController;
 
     // GET: /scrapper/api/links
     @Test
@@ -164,7 +48,7 @@ public class ApiControllerTest {
     @Test
     @DisplayName("Test correct link get to /links with manually occurred Exception")
     public void test10() throws Exception {
-        when(controllers.handleGetLinks(any(Long.class)))
+        when(linksController.handleGetLinks(any(Long.class)))
             .thenAnswer(invocation -> {
                 throw new Exception();
             });
@@ -182,7 +66,7 @@ public class ApiControllerTest {
     @Test
     @DisplayName("Test correct link get to /links with manually occurred LinkNotExistsException")
     public void test11() throws Exception {
-        when(controllers.handleGetLinks(any(Long.class)))
+        when(linksController.handleGetLinks(any(Long.class)))
             .thenAnswer(invocation -> {
                 throw new LinkNotExistsException();
             });
@@ -227,7 +111,7 @@ public class ApiControllerTest {
     @Test
     @DisplayName("Test correct link delete to /links with manually occurred Exception")
     public void test14() throws Exception {
-        when(controllers.handleDeleteLinks(any(Long.class)))
+        when(linksController.handleDeleteLinks(any(Long.class)))
             .thenAnswer(invocation -> {
                 throw new Exception();
             });
@@ -245,7 +129,7 @@ public class ApiControllerTest {
     @Test
     @DisplayName("Test correct link delete to /links with manually occurred LinkNotExistsException")
     public void test15() throws Exception {
-        when(controllers.handleDeleteLinks(any(Long.class)))
+        when(linksController.handleDeleteLinks(any(Long.class)))
             .thenAnswer(invocation -> {
                 throw new LinkNotExistsException();
             });

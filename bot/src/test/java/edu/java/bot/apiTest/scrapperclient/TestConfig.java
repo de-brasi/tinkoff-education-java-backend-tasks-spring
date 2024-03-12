2b9c216ebdb12b4146444fcc31ace47af60d6508
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-12 14:19:25 +0300

    manually creating bean with ObjectMapper in use it in ScrapperClient's constructor

--- bot/src/test/java/edu/java/bot/apiTest/scrapperclient/TestConfig.java
+++ bot/src/test/java/edu/java/bot/apiTest/scrapperclient/TestConfig.java
@@ -1,11 +1,18 @@
 package edu.java.bot.apiTest.scrapperclient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.java.bot.client.ScrapperClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 public class TestConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
     @Bean("scrapperTestClient")
-    public ScrapperClient scrapperTestClient() {
-        return new ScrapperClient("http://localhost:8080/scrapperMock/");
+    public ScrapperClient scrapperTestClient(@Autowired ObjectMapper mapper) {
+        return new ScrapperClient("http://localhost:8080/scrapperMock/", mapper);
     }
 }

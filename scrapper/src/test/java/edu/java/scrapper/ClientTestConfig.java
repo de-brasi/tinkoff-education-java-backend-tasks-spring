cf3c9055f3e5acca78022524dcb60d4ccf2afa0a
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-12 11:49:23 +0300

    discard todo "timeout value from config"

--- scrapper/src/test/java/edu/java/scrapper/ClientTestConfig.java
+++ scrapper/src/test/java/edu/java/scrapper/ClientTestConfig.java
@@ -7,13 +7,11 @@ import org.springframework.context.annotation.Bean;
 public class ClientTestConfig {
     @Bean("testGitHubClient")
     public GitHubClient gitHubClient() {
-        // todo: timeout value from config
         return new GitHubClient("http://localhost:8080/repos/", 1000);
     }
 
     @Bean("testStackOverflowClient")
     public StackOverflowClient stackoverflowClient() {
-        // todo: timeout value from config
         return new StackOverflowClient("http://localhost:8080/questions/", 1000);
     }
 }

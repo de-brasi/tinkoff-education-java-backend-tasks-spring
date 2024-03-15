5c4b014e5c88b01a71b71acb6d40514567ed5d21
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-15 19:33:43 +0300

    autowire TgChatService to constructor; using TgChatService in handlers

--- scrapper/src/main/java/edu/java/api/ChatController.java
+++ scrapper/src/main/java/edu/java/api/ChatController.java
@@ -1,8 +1,11 @@
 package edu.java.api;
 
 import edu.common.dtos.ApiErrorResponse;
+import edu.java.services.interfaces.TgChatService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/scrapper/api/tg-chat", produces = MediaType.APPLICATION_JSON_VALUE)
 @SuppressWarnings({"MultipleStringLiterals"})
 public class ChatController {
+    private final TgChatService tgChatService;
+
+    public ChatController(@Autowired TgChatService tgChatService) {
+        this.tgChatService = tgChatService;
+
+    }
+
     @PostMapping(value = "/{id}")
     public ResponseEntity<ApiErrorResponse> handleRegistryChat(@PathVariable Long id) {
         // todo проверять на:
         //  - некорректные параметры 400
-        LOGGER.info(id);
+
+        tgChatService.register(id);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -30,7 +41,8 @@ public class ChatController {
         // todo проверять на:
         //  - некорректные параметры 400
         //  - чат не существует 404
-        LOGGER.info(id);
+
+        tgChatService.unregister(id);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }

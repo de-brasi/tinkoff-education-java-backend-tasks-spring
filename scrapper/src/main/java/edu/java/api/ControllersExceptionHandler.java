ff9fdfbcc6b4060a88c8182d125e625a8e15dcde
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-15 18:07:18 +0300

    add logger

--- scrapper/src/main/java/edu/java/api/ControllersExceptionHandler.java
+++ scrapper/src/main/java/edu/java/api/ControllersExceptionHandler.java
@@ -6,6 +6,8 @@ import edu.common.exceptions.LinkNotExistsException;
 import edu.common.exceptions.ReAddingLinkException;
 import edu.common.exceptions.ReRegistrationException;
 import java.util.Arrays;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -133,4 +135,5 @@ public class ControllersExceptionHandler {
         );
     }
 
+    private final static Logger LOGGER = LogManager.getLogger();
 }

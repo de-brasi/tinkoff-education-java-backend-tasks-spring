f48aff534a0c088020760af9c31d185548cfaee3
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-16 17:14:08 +0300

    add usage hints in todo!

--- scrapper/src/main/java/edu/java/domain/exceptions/DataBaseInteractingException.java
+++ scrapper/src/main/java/edu/java/domain/exceptions/DataBaseInteractingException.java
@@ -1,5 +1,6 @@
 package edu.java.domain.exceptions;
 
+// todo: это не во что не мапить - отловится RestControllerAdvice'ом
 public class DataBaseInteractingException extends RuntimeException {
     public DataBaseInteractingException(String errorMessage) {
         super(errorMessage);
--- scrapper/src/main/java/edu/java/domain/exceptions/NoExpectedEntityInDataBaseException.java
+++ scrapper/src/main/java/edu/java/domain/exceptions/NoExpectedEntityInDataBaseException.java
@@ -1,5 +1,6 @@
 package edu.java.domain.exceptions;
 
+// Это маппить в ChatIdNotExistsException
 public class NoExpectedEntityInDataBaseException extends RuntimeException {
     public NoExpectedEntityInDataBaseException(String entityTypeName) {
         super("No entity '%s' with expected description found in database!".formatted(entityTypeName));

3aa593c791fdf5b688f783890f86ab133c1203a8
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-17 17:18:10 +0300

    logging via log4j.Logger

--- scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -1,5 +1,7 @@
 package edu.java;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,8 @@ public class LinkUpdaterScheduler {
     @SuppressWarnings("RegexpSinglelineJava")
     @Scheduled(fixedDelayString = "#{@scheduler.interval()}")
     public void update() {
-        System.out.println("Update");
+        LOGGER.info("Update");
     }
+
+    private final static Logger LOGGER = LogManager.getLogger();
 }

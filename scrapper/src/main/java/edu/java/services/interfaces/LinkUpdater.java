b8d58baa1d4140d483793a573409c79ea54ddcc2
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-17 19:30:31 +0300

    better api

--- scrapper/src/main/java/edu/java/services/interfaces/LinkUpdater.java
+++ scrapper/src/main/java/edu/java/services/interfaces/LinkUpdater.java
@@ -1,5 +1,17 @@
 package edu.java.services.interfaces;
 
+import edu.java.domain.entities.Link;
+import edu.java.domain.entities.TelegramChat;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.Collection;
+
 public interface LinkUpdater {
-    int update();
+    int update(Collection<Link> toUpdate);
+
+    Collection<Link> getNotCheckedForAWhile(Duration duration);
+
+    Collection<TelegramChat> getSubscribers(Link link);
+
+    boolean compareAndSetLastUpdateTime(Link target, OffsetDateTime actualTime);
 }

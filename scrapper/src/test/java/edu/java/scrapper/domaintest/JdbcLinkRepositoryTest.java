5f0058ed00a80767eba4551fa7902da39ce0d1f1
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-18 21:27:31 +0300

    delete extra import

--- scrapper/src/test/java/edu/java/scrapper/domaintest/JdbcLinkRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/domaintest/JdbcLinkRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import java.net.MalformedURLException;
 import java.net.URI;

05321e32245cae404e14449478769fdaf9d1a174
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-18 16:10:25 +0300

    better interface; delete methods which should not be public

--- scrapper/src/main/java/edu/java/services/interfaces/LinkUpdater.java
+++ scrapper/src/main/java/edu/java/services/interfaces/LinkUpdater.java
@@ -1,18 +1,7 @@
 package edu.java.services.interfaces;
 
-import edu.java.domain.entities.Link;
-import edu.java.domain.entities.TelegramChat;
-import java.net.MalformedURLException;
 import java.time.Duration;
-import java.time.OffsetDateTime;
-import java.util.Collection;
 
 public interface LinkUpdater {
-    int update(Collection<Link> toUpdate);
-
-    Collection<Link> getNotCheckedForAWhile(Duration duration);
-
-    Collection<TelegramChat> getSubscribers(Link link);
-
-    boolean compareAndSetLastUpdateTime(Link target, OffsetDateTime actualTime) throws MalformedURLException;
+    int update(Duration updateInterval);
 }

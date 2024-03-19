337d5aa6f2fab36c8dd5db71456a32e6f71a44f3
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-19 16:55:58 +0300

    init

---
+++ scrapper/src/main/java/edu/java/clients/ExternalServiceClient.java
@@ -0,0 +1,11 @@
+package edu.java.clients;
+
+import edu.java.clients.entities.UpdateResponse;
+import edu.java.clients.exceptions.EmptyResponseBodyException;
+import edu.java.clients.exceptions.FieldNotFoundException;
+
+public interface ExternalServiceClient {
+    boolean checkURLSupportedByService(String url);
+
+    UpdateResponse fetchUpdate(String url) throws EmptyResponseBodyException, FieldNotFoundException;
+}

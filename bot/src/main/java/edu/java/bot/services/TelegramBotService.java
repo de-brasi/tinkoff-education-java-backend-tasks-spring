1a70f8ac91f3ef36434e4970d8e5fd1800d3f090
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-22 14:17:34 +0300

    use beans with commands from UsedCommandsConfig instead of using builders from ReadyToUseCommands class

--- bot/src/main/java/edu/java/bot/services/TelegramBotService.java
+++ bot/src/main/java/edu/java/bot/services/TelegramBotService.java
@@ -1,27 +1,51 @@
 package edu.java.bot.services;
 
 import com.pengrad.telegrambot.ExceptionHandler;
-import edu.java.bot.core.commands.ReadyToUseCommands;
-import edu.java.bot.repository.interfaces.UsersRepository;
+import edu.java.bot.core.commands.TelegramBotCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TelegramBotService {
 
-    public TelegramBotService(@Autowired TelegramBotWrapper bot, @Autowired UsersRepository usersRepository) {
+    public TelegramBotService(
+        @Autowired
+        TelegramBotWrapper bot,
+
+        @Autowired
+        @Qualifier("unexpectedCommand")
+        TelegramBotCommand unexpected,
+
+        @Autowired
+        @Qualifier("commandStart")
+        TelegramBotCommand start,
+
+        @Autowired
+        @Qualifier("commandHelp")
+        TelegramBotCommand help,
+
+        @Autowired
+        @Qualifier("commandTrack")
+        TelegramBotCommand track,
+
+        @Autowired
+        @Qualifier("commandUntrack")
+        TelegramBotCommand untrack,
+
+        @Autowired
+        @Qualifier("commandList")
+        TelegramBotCommand list
+    ) {
+
         LinkTrackerObserver listener = new LinkTrackerObserver(bot);
         listener.setCommands(
-            ReadyToUseCommands.unexpectedCommand(),
-            ReadyToUseCommands.start(),
-            ReadyToUseCommands.help(),
-            ReadyToUseCommands.track(usersRepository),
-            ReadyToUseCommands.untrack(usersRepository),
-            ReadyToUseCommands.list(usersRepository)
+            unexpected, start,
+            help, track,
+            untrack, list
         );
-
         ExceptionHandler errorHandler = new LinkTrackerErrorHandler();
-
         bot.setUpdatesListener(listener, errorHandler);
+
     }
 }

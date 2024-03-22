21a23db3c4165fdc9c6f0ba517606a7a16764013
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-22 22:36:44 +0300

    init

---
+++ scrapper/src/main/java/edu/java/domain/repositories/jpa/entities/TelegramChat.java
@@ -0,0 +1,25 @@
+package edu.java.domain.repositories.jpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "telegram_chat")
+@NoArgsConstructor
+@Setter
+@Getter
+public class TelegramChat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private Long chatId;
+}

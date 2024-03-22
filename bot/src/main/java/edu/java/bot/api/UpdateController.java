6b966aa9b8949a993580b15e2109b52e7c3fa7e8
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-22 15:25:25 +0300

    send message to clients about updates

--- bot/src/main/java/edu/java/bot/api/UpdateController.java
+++ bot/src/main/java/edu/java/bot/api/UpdateController.java
@@ -2,8 +2,10 @@ package edu.java.bot.api;
 
 import edu.common.dtos.ApiErrorResponse;
 import edu.common.dtos.LinkUpdateRequest;
+import edu.java.bot.services.TelegramBotWrapper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +17,25 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/bot/api")
 public class UpdateController {
+    public UpdateController(@Autowired TelegramBotWrapper telegramBotWrapper) {
+        this.telegramBot = telegramBotWrapper;
+    }
+
+    private final TelegramBotWrapper telegramBot;
+
     @PostMapping(value = "/updates", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @SuppressWarnings("RegexpSinglelineJava")
     public ResponseEntity<ApiErrorResponse> handleUpdateRequest(@RequestBody LinkUpdateRequest request) {
-        LOGGER.info(request);
+        LOGGER.info("Update request: " + request);
+
+        final String messageToClient = "Update in link %s with description: '%s'"
+            .formatted(request.getUrl(), request.getDescription());
+
+        for (Long chatId : request.getTgChatIds()) {
+            // todo: обработка ошибок
+            telegramBot.sendPlainTextMessage(chatId, messageToClient);
+        }
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 

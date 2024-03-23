a95c1cfc87e48517fc488594d6dee11447cd2d22
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-23 23:05:59 +0300

    init

---
+++ scrapper/src/main/java/edu/java/configuration/JpaAccessConfiguration.java
@@ -0,0 +1,41 @@
+package edu.java.configuration;
+
+import edu.java.clients.BotClient;
+import edu.java.domain.repositories.jpa.implementations.JpaLinkRepository;
+import edu.java.domain.repositories.jpa.implementations.JpaTelegramChatRepository;
+import edu.java.services.ExternalServicesObserver;
+import edu.java.services.interfaces.LinkService;
+import edu.java.services.interfaces.LinkUpdater;
+import edu.java.services.interfaces.TgChatService;
+import edu.java.services.jpa.JpaLinkService;
+import edu.java.services.jpa.JpaLinkUpdater;
+import edu.java.services.jpa.JpaTgChatService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "app", name = "database-access-type", havingValue = "jpa")
+public class JpaAccessConfiguration {
+    @Bean
+    public LinkService linkService(
+        JpaLinkRepository jpaLinkRepository,
+        JpaTelegramChatRepository jpaTelegramChatRepository
+    ) {
+        return new JpaLinkService(jpaLinkRepository, jpaTelegramChatRepository);
+    }
+
+    @Bean
+    public LinkUpdater linkUpdater(
+        JpaLinkRepository linkRepository,
+        ExternalServicesObserver externalServicesObserver,
+        BotClient botClient
+    ) {
+        return new JpaLinkUpdater(linkRepository, externalServicesObserver, botClient);
+    }
+
+    @Bean
+    public TgChatService tgChatService(JpaTelegramChatRepository jpaTelegramChatRepository) {
+        return new JpaTgChatService(jpaTelegramChatRepository);
+    }
+}

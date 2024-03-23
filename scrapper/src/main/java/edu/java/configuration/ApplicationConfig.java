cf7685a463cf0fb3be90268257967f0fab063119
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-23 22:26:22 +0300

    add databaseAccessType field

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -19,11 +19,18 @@ public record ApplicationConfig(
 
     @NotNull
     @Bean("stackoverflowClientSettings")
-    ClientSettings stackoverflowClientSettings
+    ClientSettings stackoverflowClientSettings,
+
+    AccessType databaseAccessType
 ) {
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
     }
 
     public record ClientSettings(int timeoutInMilliseconds){
+    }
+
+    public enum AccessType {
+        JDBC,
+        JPA,
     }
 }

f692bccada3f69fe9b75cb6619e1782b09c362a1
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-23 12:32:43 +0300

    add lombok getters and setters

--- scrapper/src/main/java/edu/java/domain/repositories/jpa/entities/TrackInfo.java
+++ scrapper/src/main/java/edu/java/domain/repositories/jpa/entities/TrackInfo.java
@@ -7,11 +7,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "track_info")
 @NoArgsConstructor
+@Getter
+@Setter
 public class TrackInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

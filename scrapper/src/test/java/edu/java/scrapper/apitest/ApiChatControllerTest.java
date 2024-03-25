3b1f25fd196c34d93727ff8a08c0a43c95f4feed
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-25 20:29:58 +0300

    better

--- scrapper/src/test/java/edu/java/scrapper/apitest/ApiChatControllerTest.java
+++ scrapper/src/test/java/edu/java/scrapper/apitest/ApiChatControllerTest.java
@@ -7,9 +7,7 @@ import edu.java.api.LinksController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,8 +17,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest
-//@AutoConfigureMockMvc
 @WebMvcTest(ChatController.class)
 public class ApiChatControllerTest {
 
--- scrapper/src/test/java/edu/java/scrapper/apitest/ApiLinksControllerTest.java
+++ scrapper/src/test/java/edu/java/scrapper/apitest/ApiLinksControllerTest.java
@@ -1,26 +1,20 @@
 package edu.java.scrapper.apitest;
 
-import edu.common.dtos.RemoveLinkRequest;
 import edu.common.exceptions.LinksNotAddedException;
 import edu.java.api.ChatController;
 import edu.java.api.LinksController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest
-//@AutoConfigureMockMvc
 @WebMvcTest(LinksController.class)
 public class ApiLinksControllerTest {
 

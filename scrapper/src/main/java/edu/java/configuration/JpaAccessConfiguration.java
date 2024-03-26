7a3d2ae46ce39ba32f22e4d66e0b703bb811301a
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-26 21:27:13 +0300

    add JpaSupportedServicesRepository for new JpaLinkService constructor

--- scrapper/src/main/java/edu/java/configuration/JpaAccessConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/JpaAccessConfiguration.java
@@ -2,6 +2,7 @@ package edu.java.configuration;
 
 import edu.java.clients.BotClient;
 import edu.java.domain.repositories.jpa.implementations.JpaLinkRepository;
+import edu.java.domain.repositories.jpa.implementations.JpaSupportedServicesRepository;
 import edu.java.domain.repositories.jpa.implementations.JpaTelegramChatRepository;
 import edu.java.services.ExternalServicesObserver;
 import edu.java.services.interfaces.LinkService;
@@ -20,9 +21,10 @@ public class JpaAccessConfiguration {
     @Bean
     public LinkService linkService(
         JpaLinkRepository jpaLinkRepository,
-        JpaTelegramChatRepository jpaTelegramChatRepository
+        JpaTelegramChatRepository jpaTelegramChatRepository,
+        JpaSupportedServicesRepository jpaSupportedServicesRepository
     ) {
-        return new JpaLinkService(jpaLinkRepository, jpaTelegramChatRepository);
+        return new JpaLinkService(jpaLinkRepository, jpaTelegramChatRepository, jpaSupportedServicesRepository);
     }
 
     @Bean

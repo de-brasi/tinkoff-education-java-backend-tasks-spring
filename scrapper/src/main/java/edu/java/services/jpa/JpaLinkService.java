3c2ed68df7b9744e00e6abbdade77e1f99e166cf
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-26 21:28:22 +0300

    bugfix; check when link is null (and create if so)

--- scrapper/src/main/java/edu/java/services/jpa/JpaLinkService.java
+++ scrapper/src/main/java/edu/java/services/jpa/JpaLinkService.java
@@ -1,9 +1,11 @@
 package edu.java.services.jpa;
 
 import edu.java.domain.entities.Link;
+import edu.java.domain.repositories.jpa.entities.SupportedService;
 import edu.java.domain.repositories.jpa.entities.TelegramChat;
 import edu.java.domain.repositories.jpa.entities.TrackInfo;
 import edu.java.domain.repositories.jpa.implementations.JpaLinkRepository;
+import edu.java.domain.repositories.jpa.implementations.JpaSupportedServicesRepository;
 import edu.java.domain.repositories.jpa.implementations.JpaTelegramChatRepository;
 import edu.java.services.interfaces.LinkService;
 import jakarta.persistence.EntityManager;
@@ -17,9 +19,14 @@ import java.util.stream.Collectors;
 import org.springframework.transaction.annotation.Transactional;
 
 public class JpaLinkService implements LinkService {
-    public JpaLinkService(JpaLinkRepository jpaLinkRepository, JpaTelegramChatRepository jpaTelegramChatRepository) {
+    public JpaLinkService(
+        JpaLinkRepository jpaLinkRepository,
+        JpaTelegramChatRepository jpaTelegramChatRepository,
+        JpaSupportedServicesRepository servicesRepository
+    ) {
         this.linkRepository = jpaLinkRepository;
         this.chatRepository = jpaTelegramChatRepository;
+        this.servicesRepository = servicesRepository;
     }
 
     @PersistenceContext
@@ -27,6 +34,7 @@ public class JpaLinkService implements LinkService {
 
     private final JpaLinkRepository linkRepository;
     private final JpaTelegramChatRepository chatRepository;
+    private final JpaSupportedServicesRepository servicesRepository;
 
     @Override
     @Transactional
@@ -34,6 +42,12 @@ public class JpaLinkService implements LinkService {
         try {
             TelegramChat chat = chatRepository.get(tgChatId);
             edu.java.domain.repositories.jpa.entities.Link link = linkRepository.get(url.toURL().toString());
+
+            if (link == null) {
+                linkRepository.add(url.toURL().toString(), getLinksService(url));
+                link = linkRepository.get(url.toURL().toString());
+            }
+
             TrackInfo trackInfo = new TrackInfo();
             trackInfo.setLink(link);
             trackInfo.setChat(chat);
@@ -82,5 +96,17 @@ public class JpaLinkService implements LinkService {
                 )
             )
             .collect(Collectors.toList());
+    }
+
+    private SupportedService getLinksService(URI url) throws MalformedURLException {
+        String urlString = url.toURL().toString();
+
+        if (urlString.startsWith("https://stackoverflow.com/")) {
+            return servicesRepository.getService("stackoverflow");
+        } else if (urlString.startsWith("https://github.com/")) {
+            return servicesRepository.getService("github");
+        } else {
+            throw new RuntimeException("Unexpected link %s; cant find relative service".formatted(urlString));
+        }
     }
 }

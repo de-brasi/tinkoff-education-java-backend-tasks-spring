0f32b4b3d31c076959e26eb00134218fb09cf4ca
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-27 22:01:50 +0300

    init

---
+++ bot/src/main/java/edu/java/bot/retryutils/Retry.java
@@ -0,0 +1,15 @@
+package edu.java.bot.retryutils;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Retry {
+    long delay() default 0;
+    int attempts() default 0;
+    Class<? extends Exception>[] handled();
+    BackoffPolicy backoffPolicy() default BackoffPolicy.LINEAR;
+}

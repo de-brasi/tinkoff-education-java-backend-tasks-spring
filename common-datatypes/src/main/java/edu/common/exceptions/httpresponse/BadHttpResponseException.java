9d0bc1e6082295b412b25f4e777c4ac9e12578b9
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-28 17:09:42 +0300

    init

---
+++ common-datatypes/src/main/java/edu/common/exceptions/httpresponse/BadHttpResponseException.java
@@ -0,0 +1,17 @@
+package edu.common.exceptions.httpresponse;
+
+import edu.common.dtos.ApiErrorResponse;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BadHttpResponseException extends RuntimeException {
+    public BadHttpResponseException(@NotNull HttpStatus httpCode, @NotNull ApiErrorResponse errorResponse) {
+        this.httpCode = httpCode;
+        this.errorResponse = errorResponse;
+    }
+
+    private final HttpStatus httpCode;
+    private final ApiErrorResponse errorResponse;
+}

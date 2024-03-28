cb8404354cbcf75b4aa607ff0f974635bcf2135e
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-28 13:32:09 +0300

    add initialDelay field

--- bot/src/main/java/edu/java/bot/retryutils/Retry.java
+++ bot/src/main/java/edu/java/bot/retryutils/Retry.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Retry {
-    long delay() default 0;
+    long initialDelay() default 0;
     int attempts() default 0;
     Class<? extends Exception>[] handled();
     BackoffPolicy backoffPolicy() default BackoffPolicy.LINEAR;

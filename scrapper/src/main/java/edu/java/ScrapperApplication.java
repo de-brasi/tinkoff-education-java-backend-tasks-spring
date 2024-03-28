8270f3263f38e6f92e3aa50026a33b9bd8a27a04
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-28 19:03:14 +0300

    import RetryAspect component

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -1,12 +1,15 @@
 package edu.java;
 
+import edu.common.RetryAspect;
 import edu.java.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
+@Import(RetryAspect.class)
 public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);

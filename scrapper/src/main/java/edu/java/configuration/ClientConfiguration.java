b4cdd107b14c70a3eb6a178dd1bdf3790abc564e
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-28 19:14:39 +0300

    add ErrorHandler bean and pass to StackOverflowClient and GitHubClient constructor

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -1,24 +1,59 @@
 package edu.java.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.common.dtos.ApiErrorResponse;
+import edu.common.exceptions.httpresponse.BadHttpResponseException;
 import edu.java.clients.GitHubClient;
 import edu.java.clients.StackOverflowClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestClient;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class ClientConfiguration {
+    private static final Charset DEFAULT_BODY_ENCODING = StandardCharsets.UTF_8;
+
+    @Bean("notOkResponseHandler")
+    RestClient.ResponseSpec.ErrorHandler notOkResponseHandler(@Autowired ObjectMapper objectMapper) {
+        return (req, resp) -> {
+            ApiErrorResponse errorResponse = objectMapper.readValue(
+                new String(resp.getBody().readAllBytes(), DEFAULT_BODY_ENCODING),
+                ApiErrorResponse.class
+            );
+
+            HttpStatus status = HttpStatus.valueOf(resp.getStatusCode().value());
+
+            throw new BadHttpResponseException(status, errorResponse);
+        };
+    }
+
     @Bean("gitHubClient")
     public GitHubClient gitHubClient(
-        @Value("#{@githubClientSettings.timeoutInMilliseconds()}") int timeoutInMilliseconds
+        @Value("#{@githubClientSettings.timeoutInMilliseconds()}")
+        int timeoutInMilliseconds,
+
+        @Autowired
+        @Qualifier("notOkResponseHandler")
+        RestClient.ResponseSpec.ErrorHandler notOkResponseHandler
     ) {
-        return new GitHubClient("https://api.github.com/repos/", timeoutInMilliseconds);
+        return new GitHubClient("https://api.github.com/repos/", timeoutInMilliseconds, notOkResponseHandler);
     }
 
     @Bean("stackOverflowClient")
     public StackOverflowClient stackoverflowClient(
-        @Value("#{@stackoverflowClientSettings.timeoutInMilliseconds()}") int timeoutInMilliseconds
+        @Value("#{@stackoverflowClientSettings.timeoutInMilliseconds()}")
+        int timeoutInMilliseconds,
+
+        @Autowired
+        @Qualifier("notOkResponseHandler")
+        RestClient.ResponseSpec.ErrorHandler notOkResponseHandler
     ) {
-        return new StackOverflowClient(timeoutInMilliseconds);
+        return new StackOverflowClient(timeoutInMilliseconds, notOkResponseHandler);
     }
 }

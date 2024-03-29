c7ecf56b27bf14f45e0fc594d6a35229022849cc
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-29 18:47:39 +0300

    remove logger

--- scrapper/src/main/java/edu/java/api/LinksController.java
+++ scrapper/src/main/java/edu/java/api/LinksController.java
@@ -16,8 +16,6 @@ import java.util.stream.Collectors;
 import io.github.bucket4j.Bucket;
 import io.github.bucket4j.ConsumptionProbe;
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -118,7 +116,5 @@ public class LinksController {
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
-    private final static Logger LOGGER = LogManager.getLogger();
 }
 

90370eeab3e1fcd536dd6a0a7b51ce26a76303f6
de-brasi <leskin.ilia.public@yandex.ru>
2024-03-30 13:59:57 +0300

    add request rate limiter via class RequestRateSupervisor from module "common"; delete unnecessary LOGGER

--- bot/src/main/java/edu/java/bot/api/UpdateController.java
+++ bot/src/main/java/edu/java/bot/api/UpdateController.java
@@ -2,9 +2,11 @@ package edu.java.bot.api;
 
 import edu.common.datatypes.dtos.ApiErrorResponse;
 import edu.common.datatypes.dtos.LinkUpdateRequest;
+import edu.common.ratelimiting.RequestRateSupervisor;
 import edu.java.bot.services.TelegramBotWrapper;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.ConsumptionProbe;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,27 +19,40 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RestController
 @RequestMapping("/bot/api")
 public class UpdateController {
-    public UpdateController(@Autowired TelegramBotWrapper telegramBotWrapper) {
-        this.telegramBot = telegramBotWrapper;
-    }
-
     private final TelegramBotWrapper telegramBot;
+    private final RequestRateSupervisor requestRateSupervisor;
+    private static final ResponseEntity<?> REQUEST_RATE_LIMIT_ACHIEVED_RESPONSE = new ResponseEntity<>(
+        new ApiErrorResponse(
+            "rate limit", "429", null, null, null
+        ),
+        HttpStatus.TOO_MANY_REQUESTS
+    );
+
+    public UpdateController(
+        @Autowired TelegramBotWrapper telegramBotWrapper,
+        @Autowired RequestRateSupervisor supervisor
+    ) {
+        this.telegramBot = telegramBotWrapper;
+        this.requestRateSupervisor = supervisor;
+    }
 
     @PostMapping(value = "/updates", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @SuppressWarnings("RegexpSinglelineJava")
-    public ResponseEntity<ApiErrorResponse> handleUpdateRequest(@RequestBody LinkUpdateRequest request) {
-        LOGGER.info("Update request: " + request);
+    public ResponseEntity<?> handleUpdateRequest(@RequestBody LinkUpdateRequest requestBody, HttpServletRequest request) {
+        Bucket bucket = requestRateSupervisor.resolveBucket(request.getRemoteAddr());
+        ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
+
+        if (!probe.isConsumed()) {
+            return REQUEST_RATE_LIMIT_ACHIEVED_RESPONSE;
+        }
 
         final String messageToClient = "Update in link %s with description: '%s'"
-            .formatted(request.getUrl(), request.getDescription());
+            .formatted(requestBody.getUrl(), requestBody.getDescription());
 
-        for (Long chatId : request.getTgChatIds()) {
-            // todo: обработка ошибок
+        for (Long chatId : requestBody.getTgChatIds()) {
             telegramBot.sendPlainTextMessage(chatId, messageToClient);
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
-    private final static Logger LOGGER = LogManager.getLogger();
 }

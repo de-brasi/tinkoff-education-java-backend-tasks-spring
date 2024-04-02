9d935b8b385d01b8d17d7ba94f2bc0bbaec11edd
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-02 21:20:39 +0300

    better constructor (via @RequiredArgsConstructor); log stub for consumer presence

--- scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -2,25 +2,28 @@ package edu.java;
 
 import edu.java.services.interfaces.LinkUpdater;
 import java.time.Duration;
+import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @EnableScheduling
+@RequiredArgsConstructor
 public class LinkUpdaterScheduler {
     private final LinkUpdater linkUpdater;
-
-    public LinkUpdaterScheduler(@Autowired LinkUpdater linkUpdater) {
-        this.linkUpdater = linkUpdater;
-    }
+    private final KafkaTemplate<String, String> template;
 
     @SuppressWarnings("RegexpSinglelineJava")
     @Scheduled(fixedDelayString = "#{@scheduler.interval()}")
     public void update() {
+        // kafka message
+        LOGGER.warn("Send message to topic '%s' with data '%s'".formatted("topic1", "test"));
+        template.send("topic1", "test");
+
         LOGGER.info("Update");
 
         Duration checkingDeadline = Duration.ofMinutes(1);

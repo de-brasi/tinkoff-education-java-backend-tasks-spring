2ea037a9cf9d878318c21cf2700b30c9ac118c3a
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-03 19:56:57 +0300

    init

---
+++ scrapper/src/main/java/edu/java/configuration/UpdateProducerConfig.java
@@ -0,0 +1,32 @@
+package edu.java.configuration;
+
+import edu.java.clients.BotClient;
+import edu.java.updateproducing.ScrapperHttpProducer;
+import edu.java.updateproducing.ScrapperQueueProducer;
+import edu.java.updateproducing.ScrapperUpdateProducer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Configuration
+public class UpdateProducerConfig {
+    @Bean("scrapperKafkaProducer")
+    ScrapperUpdateProducer scrapperKafkaProducer(
+        @Value("#{@kafkaTopic}")
+        String topicName,
+
+        KafkaTemplate<String, String> kafkaTemplate
+    ) {
+        return new ScrapperQueueProducer(topicName, kafkaTemplate);
+    }
+
+    @Bean("scrapperHttpProducer")
+    ScrapperUpdateProducer scrapperHttpProducer(
+        @Value("#{@kafkaTopic}") String topicName,
+
+        BotClient botClient
+    ) {
+        return new ScrapperHttpProducer(topicName, botClient);
+    }
+}

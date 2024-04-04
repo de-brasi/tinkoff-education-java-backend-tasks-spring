83d73aba50b0630af941036b979813f97677a4e6
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-04 14:45:02 +0300

    clear extra code

--- scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/LinkUpdaterScheduler.java
@@ -3,9 +3,7 @@ package edu.java;
 import edu.java.services.interfaces.LinkUpdater;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.kafka.core.KafkaTemplate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -13,24 +11,18 @@ import org.springframework.stereotype.Service;
 @Service
 @EnableScheduling
 @RequiredArgsConstructor
+@Slf4j
 public class LinkUpdaterScheduler {
     private final LinkUpdater linkUpdater;
-    private final KafkaTemplate<String, String> template;
 
     @SuppressWarnings("RegexpSinglelineJava")
     @Scheduled(fixedDelayString = "#{@scheduler.interval()}")
     public void update() {
-        // kafka message
-        LOGGER.warn("Send message to topic '%s' with data '%s'".formatted("topic1", "test"));
-        template.send("topic1", "test");
-
-        LOGGER.info("Update");
+        log.info("Update");
 
         Duration checkingDeadline = Duration.ofMinutes(1);
 
         int updated = linkUpdater.update(checkingDeadline);
-        LOGGER.info("Updated %d links.".formatted(updated));
+        log.info("Updated %d links.".formatted(updated));
     }
-
-    private final static Logger LOGGER = LogManager.getLogger();
 }

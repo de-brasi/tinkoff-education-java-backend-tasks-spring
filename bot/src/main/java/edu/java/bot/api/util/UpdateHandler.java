75b014805bc8cb074f95b1587c997ad40a37cd92
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-05 21:15:34 +0300

    send message to kafka dead letters queue if exception when message handling

--- bot/src/main/java/edu/java/bot/api/util/UpdateHandler.java
+++ bot/src/main/java/edu/java/bot/api/util/UpdateHandler.java
@@ -3,20 +3,29 @@ package edu.java.bot.api.util;
 import edu.common.datatypes.dtos.LinkUpdateRequest;
 import edu.java.bot.services.TelegramBotWrapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UpdateHandler {
     private final TelegramBotWrapper telegramBot;
+    private final KafkaTemplate<String, LinkUpdateRequest> template;
+
+    @Value("${app.dead-letter-queue-topic.name}")
+    private String faultyLettersTopicName;
 
     public void handleUpdate(LinkUpdateRequest updateRequest) {
         final String messageToClient = "Update in link %s with description: '%s'"
             .formatted(updateRequest.getUrl(), updateRequest.getDescription());
 
-        for (Long chatId : updateRequest.getTgChatIds()) {
-            // todo: какие ошибки кидает?
-            telegramBot.sendPlainTextMessage(chatId, messageToClient);
+        try {
+            for (Long chatId : updateRequest.getTgChatIds()) {
+                telegramBot.sendPlainTextMessage(chatId, messageToClient);
+            }
+        } catch (Exception e) {
+            template.send(faultyLettersTopicName, updateRequest);
         }
     }
 }

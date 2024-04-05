8d30cb46adec988dabd53c0c342ece547e2bc565
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-05 22:38:55 +0300

    better ...? (btw fix tests)

--- scrapper/src/test/java/edu/java/scrapper/domaintest/jpa/JpaLinkRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/domaintest/jpa/JpaLinkRepositoryTest.java
@@ -1,10 +1,21 @@
 package edu.java.scrapper.domaintest.jpa;
 
+import edu.java.LinkUpdaterScheduler;
+import edu.java.api.LinksController;
+import edu.java.clients.GitHubClient;
+import edu.java.clients.StackOverflowClient;
+import edu.java.configuration.KafkaConfig;
 import edu.java.domain.entities.Link;
+import edu.java.domain.repositories.jdbc.JdbcChatLinkBoundRepository;
+import edu.java.domain.repositories.jdbc.JdbcLinkRepository;
+import edu.java.domain.repositories.jdbc.JdbcTelegramChatRepository;
 import edu.java.domain.repositories.jpa.entities.SupportedService;
 import edu.java.domain.repositories.jpa.implementations.JpaLinkRepository;
 import edu.java.scrapper.IntegrationTest;
 import edu.java.services.ExternalServicesObserver;
+import edu.java.services.interfaces.LinkUpdater;
+import edu.java.updateproducing.ScrapperHttpProducer;
+import edu.java.updateproducing.ScrapperQueueProducer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -23,11 +35,55 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "app.database-access-type=jpa",
+    "app.github-client-settings.timeout-in-milliseconds=1000",
+    "app.stackoverflow-client-settings.timeout-in-milliseconds=1000",
+    "app.use-queue=false",
+    "app.topic.name='some'",
+    "app.topic.partitions-count=1",
+    "app.topic.replicas-count=2",
+    "app.scheduler.enable=false",
+    "app.scheduler.force-check-delay=1000",
+    "app.scheduler.interval=1000"
+})
 public class JpaLinkRepositoryTest extends IntegrationTest {
     @Autowired JpaLinkRepository linkRepository;
 
     @MockBean
+    LinkUpdaterScheduler linkUpdaterScheduler;
+
+    @MockBean
+    LinkUpdater linkUpdater;
+
+    @MockBean
+    LinksController linksController;
+
+    @MockBean
+    JdbcChatLinkBoundRepository jdbcChatLinkBoundRepository;
+
+    @MockBean
+    JdbcTelegramChatRepository jdbcTelegramChatRepository;
+
+    @MockBean
+    JdbcLinkRepository jdbcLinkRepository;
+
+    @MockBean
     ExternalServicesObserver externalServicesObserver;
+
+    @MockBean
+    GitHubClient gitHubClient;
+
+    @MockBean
+    StackOverflowClient stackOverflowClient;
+
+    @MockBean
+    ScrapperQueueProducer scrapperQueueProducer;
+
+    @MockBean
+    ScrapperHttpProducer scrapperHttpProducer;
+
+    @MockBean KafkaConfig kafkaConfig;
 
     @PersistenceContext
     private EntityManager entityManager;
--- scrapper/src/test/java/edu/java/scrapper/domaintest/jpa/JpaTelegramChatRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/domaintest/jpa/JpaTelegramChatRepositoryTest.java
@@ -1,22 +1,83 @@
 package edu.java.scrapper.domaintest.jpa;
 
+import edu.java.LinkUpdaterScheduler;
+import edu.java.api.LinksController;
+import edu.java.clients.GitHubClient;
+import edu.java.clients.StackOverflowClient;
+import edu.java.configuration.KafkaConfig;
+import edu.java.domain.repositories.jdbc.JdbcChatLinkBoundRepository;
+import edu.java.domain.repositories.jdbc.JdbcLinkRepository;
+import edu.java.domain.repositories.jdbc.JdbcTelegramChatRepository;
 import edu.java.domain.repositories.jpa.implementations.JpaTelegramChatRepository;
 import edu.java.scrapper.IntegrationTest;
+import edu.java.services.ExternalServicesObserver;
+import edu.java.services.interfaces.LinkUpdater;
+import edu.java.updateproducing.ScrapperHttpProducer;
+import edu.java.updateproducing.ScrapperQueueProducer;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "app.database-access-type=jpa",
+    "app.github-client-settings.timeout-in-milliseconds=1000",
+    "app.stackoverflow-client-settings.timeout-in-milliseconds=1000",
+    "app.use-queue=false",
+    "app.topic.name='some'",
+    "app.topic.partitions-count=1",
+    "app.topic.replicas-count=2",
+    "app.scheduler.enable=false",
+    "app.scheduler.force-check-delay=1000",
+    "app.scheduler.interval=1000"
+})
 public class JpaTelegramChatRepositoryTest extends IntegrationTest {
     @Autowired JpaTelegramChatRepository telegramChatRepository;
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    @MockBean
+    LinkUpdaterScheduler linkUpdaterScheduler;
+
+    @MockBean
+    LinkUpdater linkUpdater;
+
+    @MockBean
+    LinksController linksController;
+
+    @MockBean
+    JdbcChatLinkBoundRepository jdbcChatLinkBoundRepository;
+
+    @MockBean
+    JdbcTelegramChatRepository jdbcTelegramChatRepository;
+
+    @MockBean
+    JdbcLinkRepository jdbcLinkRepository;
+
+    @MockBean
+    ExternalServicesObserver externalServicesObserver;
+
+    @MockBean
+    GitHubClient gitHubClient;
+
+    @MockBean
+    StackOverflowClient stackOverflowClient;
+
+    @MockBean
+    ScrapperQueueProducer scrapperQueueProducer;
+
+    @MockBean
+    ScrapperHttpProducer scrapperHttpProducer;
+
+    @MockBean KafkaConfig kafkaConfig;
 
     @Test
     @Transactional

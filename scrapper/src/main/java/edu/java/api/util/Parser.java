81ae63596ddfc9325a8abc35b438fc7f6bb667ba
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-09 18:01:42 +0300

    init

---
+++ scrapper/src/main/java/edu/java/api/util/Parser.java
@@ -0,0 +1,40 @@
+package edu.java.api.util;
+
+import org.jetbrains.annotations.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Parser {
+
+    private final Map<String, Pattern> supportedPatterns;
+
+    public Parser() {
+        supportedPatterns = new HashMap<>();
+    }
+
+    public static ParserBuilder builder() {
+        return new ParserBuilder();
+    }
+
+    public void addField(String field, String searchRegexp) {
+        supportedPatterns.put(field, Pattern.compile(searchRegexp));
+    }
+
+    public @Nullable String retrieveValueOfField(String field, String toParse) {
+        Pattern pattern = supportedPatterns.get(field);
+
+        if (pattern == null) {
+            return null;
+        }
+
+        Matcher matcher = pattern.matcher(toParse);
+        if (!matcher.find()) {
+            return null;
+        }
+
+        return matcher.group(1);
+    }
+
+}

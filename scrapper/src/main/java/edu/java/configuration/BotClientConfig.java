eadcc19dccb2111df38b1e9a6693158cfbd1c421
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-10 14:56:49 +0300

    merge all ErrorHandler beans into one ResponseErrorHandler; use new BotClient constructor;

--- scrapper/src/main/java/edu/java/configuration/BotClientConfig.java
+++ scrapper/src/main/java/edu/java/configuration/BotClientConfig.java
@@ -5,99 +5,83 @@ import edu.common.dtos.ApiErrorResponse;
 import edu.common.exceptions.IncorrectRequestException;
 import edu.common.exceptions.UnexpectedResponse;
 import edu.java.clients.BotClient;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.client.RestClient;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
 
 @Configuration
 public class BotClientConfig {
     private static final Charset DEFAULT_BODY_ENCODING = StandardCharsets.UTF_8;
 
-    @Bean("endpointUpdatesStatus1xxHandler")
-    RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus1xxHandler(@Autowired ObjectMapper objectMapper) {
-        return (req, resp) -> {
-            ApiErrorResponse errorResponse = objectMapper.readValue(
-                new String(resp.getBody().readAllBytes(), DEFAULT_BODY_ENCODING),
-                ApiErrorResponse.class
-            );
-            throw new UnexpectedResponse(
-                resp.getStatusCode().value(),
-                errorResponse.getExceptionMessage()
-            );
-        };
-    }
+    @Bean("responseErrorHandler")
+    ResponseErrorHandler responseErrorHandler(ObjectMapper objectMapper) {
+        return new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(@NotNull ClientHttpResponse response) throws IOException {
+                return !response.getStatusCode().is2xxSuccessful();
+            }
 
-    @Bean("endpointUpdatesStatus3xxHandler")
-    RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus3xxHandler(@Autowired ObjectMapper objectMapper) {
-        return (req, resp) -> {
-            ApiErrorResponse errorResponse = objectMapper.readValue(
-                new String(resp.getBody().readAllBytes(), DEFAULT_BODY_ENCODING),
-                ApiErrorResponse.class
-            );
-            throw new UnexpectedResponse(
-                resp.getStatusCode().value(),
-                errorResponse.getExceptionMessage()
-            );
-        };
-    }
+            @Override
+            public void handleError(@NotNull ClientHttpResponse response) throws IOException {
 
-    @Bean("endpointUpdatesStatus4xxHandler")
-    RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus4xxHandler(@Autowired ObjectMapper objectMapper) {
-        return (req, resp) -> {
-            ApiErrorResponse errorResponse = objectMapper.readValue(
-                new String(resp.getBody().readAllBytes(), DEFAULT_BODY_ENCODING),
-                ApiErrorResponse.class
-            );
-            if (resp.getStatusCode() == HttpStatus.BAD_REQUEST) {
-                throw new IncorrectRequestException(errorResponse.getExceptionMessage());
-            } else {
-                throw new UnexpectedResponse(resp.getStatusCode().value(), errorResponse.getExceptionMessage());
+                var statusCode = response.getStatusCode();
+
+                byte[] bodyBytes = response.getBody().readAllBytes();
+                ApiErrorResponse errorResponse;
+                if (bodyBytes.length > 0) {
+                    errorResponse = objectMapper.readValue(
+                        new String(response.getBody().readAllBytes(), DEFAULT_BODY_ENCODING),
+                        ApiErrorResponse.class
+                    );
+                } else {
+                    errorResponse = new ApiErrorResponse(
+                        "Empty response", null, null, null, null
+                    );
+                }
+
+                if (statusCode.is1xxInformational()) {
+
+                    throw new UnexpectedResponse(
+                        response.getStatusCode().value(),
+                        errorResponse.getExceptionMessage()
+                    );
+
+                } else if (statusCode.is3xxRedirection()) {
+
+                    throw new UnexpectedResponse(
+                        response.getStatusCode().value(),
+                        errorResponse.getExceptionMessage()
+                    );
+
+                } else if (statusCode.is4xxClientError()) {
+
+                    if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                        throw new IncorrectRequestException(errorResponse.getExceptionMessage());
+                    } else {
+                        throw new UnexpectedResponse(response.getStatusCode().value(), errorResponse.getExceptionMessage());
+                    }
+
+                } else if (statusCode.is5xxServerError()) {
+
+                    throw new UnexpectedResponse(
+                        response.getStatusCode().value(),
+                        errorResponse.getExceptionMessage()
+                    );
+
+                }
+
             }
         };
     }
 
-    @Bean("endpointUpdatesStatus5xxHandler")
-    RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus5xxHandler(@Autowired ObjectMapper objectMapper) {
-        return (req, resp) -> {
-            ApiErrorResponse errorResponse = objectMapper.readValue(
-                new String(resp.getBody().readAllBytes(), DEFAULT_BODY_ENCODING),
-                ApiErrorResponse.class
-            );
-            throw new UnexpectedResponse(
-                resp.getStatusCode().value(),
-                errorResponse.getExceptionMessage()
-            );
-        };
-    }
-
     @Bean("botClient")
-    BotClient botClient(
-        @Autowired
-        @Qualifier("endpointUpdatesStatus1xxHandler")
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus1xxHandler,
-
-        @Autowired
-        @Qualifier("endpointUpdatesStatus3xxHandler")
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus3xxHandler,
-
-        @Autowired
-        @Qualifier("endpointUpdatesStatus4xxHandler")
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus4xxHandler,
-
-        @Autowired
-        @Qualifier("endpointUpdatesStatus5xxHandler")
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus5xxHandler
-    ) {
-        return new BotClient(
-            endpointUpdatesStatus1xxHandler,
-            endpointUpdatesStatus3xxHandler,
-            endpointUpdatesStatus4xxHandler,
-            endpointUpdatesStatus5xxHandler
-        );
+    BotClient botClient(ResponseErrorHandler errorHandler) {
+        return new BotClient(errorHandler);
     }
 }

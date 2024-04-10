c7b0f8ce995eaa8468e825a5d2c5ce8eb8ab7df8
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-10 17:09:44 +0300

    extend EnableConfigurationProperties arguments with GitHubClientSettings.class and StackOverflowClientSettings.class

--- scrapper/src/main/java/edu/java/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/ScrapperApplication.java
@@ -1,12 +1,18 @@
 package edu.java;
 
 import edu.java.configuration.ApplicationConfig;
+import edu.java.configuration.GitHubClientSettings;
+import edu.java.configuration.StackOverflowClientSettings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationConfig.class)
+@EnableConfigurationProperties({
+    ApplicationConfig.class,
+    GitHubClientSettings.class,
+    StackOverflowClientSettings.class
+})
 public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);

5bf342eca4106c0de4cbd47b255aa90af7629f5c
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-10 14:57:28 +0300

    use one ResponseErrorHandler instead of ErrorHandler

--- scrapper/src/main/java/edu/java/clients/BotClient.java
+++ scrapper/src/main/java/edu/java/clients/BotClient.java
@@ -1,9 +1,8 @@
 package edu.java.clients;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.common.dtos.LinkUpdateRequest;
 import java.util.List;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestClient;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
@@ -13,44 +12,24 @@ public class BotClient {
     private final RestClient restClient;
     private final static String DEFAULT_BASE_URL = "http://localhost:8090/bot/api";
     private final static String ENDPOINT_UPDATES = "/updates";
-    private final RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus1xxHandler;
-    private final RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus3xxHandler;
-    private final RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus4xxHandler;
-    private final RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus5xxHandler;
+    private final ResponseErrorHandler responseErrorHandler;
 
-    public BotClient(
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus1xxHandler,
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus3xxHandler,
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus4xxHandler,
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus5xxHandler
-    ) {
+    public BotClient(ResponseErrorHandler errorHandler) {
         this.restClient = RestClient
             .builder()
             .baseUrl(BotClient.DEFAULT_BASE_URL)
             .build();
 
-        this.endpointUpdatesStatus1xxHandler = endpointUpdatesStatus1xxHandler;
-        this.endpointUpdatesStatus3xxHandler = endpointUpdatesStatus3xxHandler;
-        this.endpointUpdatesStatus4xxHandler = endpointUpdatesStatus4xxHandler;
-        this.endpointUpdatesStatus5xxHandler = endpointUpdatesStatus5xxHandler;
+        this.responseErrorHandler = errorHandler;
     }
 
-    public BotClient(
-        String baseUrl,
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus1xxHandler,
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus3xxHandler,
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus4xxHandler,
-        RestClient.ResponseSpec.ErrorHandler endpointUpdatesStatus5xxHandler
-    ) {
+    public BotClient(String baseUrl, ResponseErrorHandler errorHandler) {
         this.restClient = RestClient
             .builder()
             .baseUrl(baseUrl)
             .build();
 
-        this.endpointUpdatesStatus1xxHandler = endpointUpdatesStatus1xxHandler;
-        this.endpointUpdatesStatus3xxHandler = endpointUpdatesStatus3xxHandler;
-        this.endpointUpdatesStatus4xxHandler = endpointUpdatesStatus4xxHandler;
-        this.endpointUpdatesStatus5xxHandler = endpointUpdatesStatus5xxHandler;
+        this.responseErrorHandler = errorHandler;
     }
 
     public void sendUpdates(
@@ -63,10 +42,7 @@ public class BotClient {
             .contentType(APPLICATION_JSON)
             .body(updates)
             .retrieve()
-            .onStatus(HttpStatusCode::is1xxInformational, endpointUpdatesStatus1xxHandler)
-            .onStatus(HttpStatusCode::is3xxRedirection, endpointUpdatesStatus3xxHandler)
-            .onStatus(HttpStatusCode::is4xxClientError, endpointUpdatesStatus4xxHandler)
-            .onStatus(HttpStatusCode::is5xxServerError, endpointUpdatesStatus5xxHandler)
+            .onStatus(responseErrorHandler)
             .toBodilessEntity();
     }
 

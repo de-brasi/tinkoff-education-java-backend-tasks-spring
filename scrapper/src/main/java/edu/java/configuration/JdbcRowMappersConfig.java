25775afe90cdfac60e5d738b7760dfa148912dea
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-15 16:49:34 +0300

    add mapper to Link object

--- scrapper/src/main/java/edu/java/configuration/JdbcRowMappersConfig.java
+++ scrapper/src/main/java/edu/java/configuration/JdbcRowMappersConfig.java
@@ -1,9 +1,11 @@
 package edu.java.configuration;
 
 import edu.java.domain.entities.ChatLinkBound;
+import edu.java.domain.entities.Link;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.RowMapper;
+import java.net.URI;
 
 @Configuration
 public class JdbcRowMappersConfig {
@@ -24,5 +26,14 @@ public class JdbcRowMappersConfig {
     @Bean
     public RowMapper<Long> chatIdRowMapper() {
         return (rs, rowNum) -> rs.getLong("chat_id");
+    }
+
+    @Bean
+    public RowMapper<Link> linkRowMapper() {
+        return (rs, rowNum) -> {
+            final String url = rs.getString("url");
+            final Long urlId = rs.getLong("link_id");
+            return new Link(urlId, URI.create(url));
+        };
     }
 }

9c125b4817e8f537b203b09734b0e66c2cafe2a4
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-23 23:33:10 +0300

    add method saveIfNotExists

--- scrapper/src/main/java/edu/java/domain/repositories/jpa/implementations/JpaTrackInfoRepository.java
+++ scrapper/src/main/java/edu/java/domain/repositories/jpa/implementations/JpaTrackInfoRepository.java
@@ -2,10 +2,23 @@ package edu.java.domain.repositories.jpa.implementations;
 
 import edu.java.domain.repositories.jpa.entities.TrackInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Collection;
 
 public interface JpaTrackInfoRepository extends JpaRepository<TrackInfo, Long> {
     @Transactional(readOnly = true)
     Collection<TrackInfo> findTrackInfoByChat_Id(Long chatId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO track_info(telegram_chat_id, link_id) VALUES (:chatId, :linkId) ON CONFLICT DO NOTHING ",
+           nativeQuery = true)
+    void saveIfNotExists(Long chatId, Long linkId);
+
+    @Transactional
+    default void saveIfNotExists(TrackInfo trackInfo) {
+        saveIfNotExists(trackInfo.getChat().getId(), trackInfo.getLink().getId());
+    }
 }

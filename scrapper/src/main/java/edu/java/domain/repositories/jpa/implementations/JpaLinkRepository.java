0f797d2b201b9f09c5d2247bfa7b464f36444d26
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-23 23:30:20 +0300

    add saveIfNotExists method; remove "add" method

--- scrapper/src/main/java/edu/java/domain/repositories/jpa/implementations/JpaLinkRepository.java
+++ scrapper/src/main/java/edu/java/domain/repositories/jpa/implementations/JpaLinkRepository.java
@@ -2,11 +2,12 @@ package edu.java.domain.repositories.jpa.implementations;
 
 import edu.java.domain.repositories.jpa.entities.Link;
 import edu.java.domain.repositories.jpa.entities.SupportedService;
-import jakarta.persistence.PersistenceException;
+import java.sql.Timestamp;
 import java.time.OffsetDateTime;
 import java.util.Optional;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,27 +15,28 @@ import org.springframework.transaction.annotation.Transactional;
 public interface JpaLinkRepository extends JpaRepository<Link, Long> {
 
     @Transactional
-    default Optional<Link> add(
+    default void saveIfNotExists(
         String url,
         SupportedService service,
         OffsetDateTime lastCheckTime,
         OffsetDateTime lastUpdateTime,
         String snapshot
     ) {
-        try {
-            Link link = new Link();
-            link.setUrl(url);
-            link.setLastCheckTime(lastCheckTime);
-            link.setLastUpdateTime(lastUpdateTime);
-            link.setService(service);
-            link.setSnapshot(snapshot);
-
-            save(link);
-            return Optional.of(link);
-        } catch (PersistenceException | DataIntegrityViolationException ignored) {
-            return Optional.empty();
-        }
+        saveIfNotExists(url, service.getId(),
+            Timestamp.from(lastCheckTime.toInstant()),
+            Timestamp.from(lastUpdateTime.toInstant()),
+            snapshot);
     }
+
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO links(url, last_check_time, last_update_time, service, snapshot) "
+        + "VALUES (:url, :lastCheckTime, :lastUpdateTime, :serviceId, :snapshot) ON CONFLICT DO NOTHING",
+           nativeQuery = true)
+    void saveIfNotExists(String url, Long serviceId,
+        Timestamp lastCheckTime,
+        Timestamp lastUpdateTime,
+        String snapshot);
 
     @Transactional
     Optional<Link> getLinkByUrl(String url);

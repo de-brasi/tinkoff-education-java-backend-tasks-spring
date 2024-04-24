e3095431a4fc64a378b650848d3af4328526f510
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-24 14:32:30 +0300

    init

---
+++ bot/src/main/java/edu/java/bot/api/Bucket4jRateLimitConfig.java
@@ -0,0 +1,21 @@
+package edu.java.bot.api;
+
+import edu.common.ratelimiting.RateLimitInterceptor;
+import edu.common.ratelimiting.RequestRateSupervisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Import({RateLimitInterceptor.class, RequestRateSupervisor.class})
+public class Bucket4jRateLimitConfig implements WebMvcConfigurer {
+    @Autowired
+    private RateLimitInterceptor interceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/**");
+    }
+}

0e8690b0d21c67e69d79dfc423dc8a411fe6ea2b
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-25 19:27:29 +0300

    creating beans depends on boolean values in kafka-settings properties

--- bot/src/main/java/edu/java/bot/configuration/KafkaConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/KafkaConfig.java
@@ -5,21 +5,26 @@ import edu.java.bot.api.util.UpdateHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@ConditionalOnProperty(prefix = "app.kafka-settings", name = "enabled", havingValue = "true")
 @RequiredArgsConstructor
 @Slf4j
 public class KafkaConfig {
     private final UpdateHandler updateHandler;
 
     @Bean
+    @ConditionalOnProperty(prefix = "app.kafka-settings.topics.scrapper-topic", name = "enabled", havingValue = "true")
     public NewTopic mainTopic(
         ApplicationConfig.KafkaSettings kafkaSettings
     ) {
+        System.out.println("main topic created");
+
         var topicSettings = kafkaSettings.topics().scrapperTopic();
 
         return TopicBuilder.name(topicSettings.name())
@@ -29,9 +34,14 @@ public class KafkaConfig {
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "app.kafka-settings.topics.dead-letter-queue-topic",
+                           name = "enabled",
+                           havingValue = "true")
     public NewTopic deadLetterQueue(
         ApplicationConfig.KafkaSettings dlqSettings
     ) {
+        System.out.println("dlq topic created");
+
         var topicSettings = dlqSettings.topics().scrapperTopic();
 
         return TopicBuilder.name(topicSettings.name())

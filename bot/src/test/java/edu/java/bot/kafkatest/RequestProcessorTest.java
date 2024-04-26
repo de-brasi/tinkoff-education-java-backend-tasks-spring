ecb8d06aaf2fb92995e3b370cf99041c42ea1d94
de-brasi <leskin.ilia.public@yandex.ru>
2024-04-26 16:54:34 +0300

    init

---
+++ bot/src/test/java/edu/java/bot/kafkatest/RequestProcessorTest.java
@@ -0,0 +1,87 @@
+package edu.java.bot.kafkatest;
+
+import edu.common.datatypes.dtos.LinkUpdateRequest;
+import edu.java.bot.api.util.UpdateHandler;
+import edu.java.bot.configuration.ApplicationConfig;
+import edu.java.bot.services.TelegramBotWrapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = {UpdateHandler.class, TelegramBotWrapper.class})
+@ExtendWith(MockitoExtension.class)
+public class RequestProcessorTest {
+    @Autowired
+    UpdateHandler updateHandler;
+
+    @MockBean
+    ApplicationConfig applicationConfig;
+
+    @MockBean
+    KafkaTemplate<String, LinkUpdateRequest> template;
+
+    @MockBean
+    TelegramBotWrapper telegramBot;
+
+    @Test
+    public void correctRequestSendNotificationsToAllClientsTest() {
+        // init
+        final List<Long> clients = List.of(1L, 2L, 3L);
+        final HashMap<Long, Integer> clientIdToReceivedMessage = clients
+            .stream()
+            .collect(Collectors.toMap(
+                    k -> k,
+                    v -> 0,
+                    (oldValue, newValue) -> newValue,
+                    HashMap::new
+                ));
+        final HashMap<Long, Integer> clientIdToReceivedMessageExpected = clients
+            .stream()
+            .collect(Collectors.toMap(
+                k -> k,
+                v -> 1,
+                (oldValue, newValue) -> newValue,
+                HashMap::new
+            ));
+        final LinkUpdateRequest linkUpdateRequest = new LinkUpdateRequest(
+            -1, "test-url", "test-description", clients);
+
+        // mock behaviour
+        doAnswer((Answer<Void>) invocation -> {
+            Long chatId = invocation.getArgument(0);
+            clientIdToReceivedMessage.put(chatId, clientIdToReceivedMessage.get(chatId) + 1);
+            return null;
+        }).when(telegramBot).sendPlainTextMessage(anyLong(), anyString());
+
+        ApplicationConfig.KafkaSettings kafkaSettings = Mockito.mock(ApplicationConfig.KafkaSettings.class);
+        when(applicationConfig.kafkaSettings()).thenReturn(kafkaSettings);
+
+        ApplicationConfig.KafkaTopicsConfig topics = Mockito.mock(ApplicationConfig.KafkaTopicsConfig.class);
+        when(kafkaSettings.topics()).thenReturn(topics);
+
+        ApplicationConfig.KafkaTopicConfig deadLetterQueueTopic = Mockito.mock(ApplicationConfig.KafkaTopicConfig.class);
+        when(topics.deadLetterQueueTopic()).thenReturn(deadLetterQueueTopic);
+
+        when(deadLetterQueueTopic.name()).thenReturn("test-topic");
+
+        // do
+        updateHandler.handleUpdate(linkUpdateRequest);
+
+        // check
+        assertThat(clientIdToReceivedMessage).isEqualTo(clientIdToReceivedMessageExpected);
+    }
+}
